2809eeb2cc39bd004b5cc904dcef519594400b8f
Khoa Phan <kp@gobackbone.com>
2016-08-02 19:39:29 -0700

    Add row onPress handler for list row

--- components/Devices.js
+++ components/Devices.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Main from './Main';
 
 import {
   View,
   Text,
   ListView,
+  NativeModules,
+  TouchableHighlight,
   NativeAppEventEmitter,
 } from 'react-native';
+
+const DeviceManagementService = NativeModules.DeviceManagementService;
 
 const styles = {
   container: {
@@ -13,7 +18,7 @@ const styles = {
   },
   title: {
     fontSize: 42,
-    marginBottom: 20,
+    marginBottom: 25,
     color: '#A8A8A8',
     fontWeight: 'bold',
     alignSelf: 'center',
@@ -36,20 +41,38 @@ class Devices extends Component {
     this.state = {
       dataSource: this.ds.cloneWithRows([]),
     };
-    this.renderRow = this.renderRow.bind(this);
+
     this.listenToDevices = NativeAppEventEmitter.addListener('Devices', (deviceList) => {
       this.setState({
         dataSource: this.ds.cloneWithRows(deviceList),
       });
     });
+
+    this.pressRow = this.pressRow.bind(this);
+    this.renderRow = this.renderRow.bind(this);
+  }
+
+  pressRow(deviceID) {
+    DeviceManagementService.selectDevice(deviceID, (error) => {
+      if (error) {
+        console.log('Error: ', error);
+      } else {
+        this.props.navigator.push({
+          name: 'main',
+          component: Main,
+        });
+      }
+    });
   }
 
   renderRow(data) {
     return (
-      <View style={styles.listItem}>
-        <Text>Name: {data.name}</Text>
-        <Text>Distance: {data.RSSI}</Text>
-      </View>
+      <TouchableHighlight onPress={() => this.pressRow(data.identifier)}>
+        <View style={styles.listItem}>
+          <Text>Name: {data.name}</Text>
+          <Text>Distance: {data.RSSI}</Text>
+        </View>
+      </TouchableHighlight>
     );
   }
 
@@ -67,5 +90,9 @@ class Devices extends Component {
     );
   }
 }
+
+Devices.propTypes = {
+  navigator: React.PropTypes.object,
+};
 
 export default Devices;
