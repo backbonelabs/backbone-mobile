9e7a03a9dec98a7885ff2ac5afe88051d538e968
Kevin Huang <kev@gobackbone.com>
2016-08-03 13:42:19 -0700

    Add PostureModule

---
+++ ios/backbone/PostureModule.h
@@ -0,0 +1,13 @@
+#import "ActivityModule.h"
+
+@interface PostureModule : ActivityModule
+@property BOOL calibrated;
+@property double controlAngle;
+@property double currentAngle;
+@property double tilt;
+@property double controlDistance;
+@property double currentDistance;
+@property double distanceThreshold;
+@property double tiltThreshold;
+- (void)handleTilt;
+@end
