0d84df971ef3146c740049b9b48ac02f69d0a782
Khoa Phan <kp@gobackbone.com>
2016-08-11 11:52:24 -0700

    Import home route and add to routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import activity from './activity';
 import devices from './devices';
+import home from './home';
 import initiate from './initiate';
 import posture from './posture';
 
-export default { activity, devices, initiate, posture };
+export default { activity, devices, home, initiate, posture };
