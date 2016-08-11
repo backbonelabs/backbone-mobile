92056bfb75de7a1b9ad26fde91c1474a81ecd8b1
Khoa Phan <kp@gobackbone.com>
2016-08-11 11:53:05 -0700

    Create styles file for Home component

---
+++ app/styles/home.js
@@ -0,0 +1,13 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+  container: {
+    marginTop: 125,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+});
