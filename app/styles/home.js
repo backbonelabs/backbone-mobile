fa1c0a4cc9c0da4df00c79d533fbc71d8c9e1d70
Khoa Phan <kp@gobackbone.com>
2016-08-11 12:39:17 -0700

    Modify style for Home component

--- app/styles/home.js
+++ app/styles/home.js
@@ -2,12 +2,26 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
   container: {
-    marginTop: 125,
     alignItems: 'center',
     justifyContent: 'center',
   },
   logo: {
     width: 300,
     height: 300,
+    marginTop: 100,
+    marginBottom: 50,
+  },
+  button: {
+    height: 75,
+    width: 300,
+    borderRadius: 4,
+    justifyContent: 'center',
+    backgroundColor: '$primaryColor',
+  },
+  buttonText: {
+    fontSize: '2rem',
+    color: 'white',
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
 });
