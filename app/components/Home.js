657348a3d33fd74a37ed49305662e719b4151881
Khoa Phan <kp@gobackbone.com>
2016-08-11 12:51:27 -0700

    Make Home component in charge of initiating and handling connection

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Text,
   Image,
+  NativeModules,
+  ActivityIndicator,
   TouchableHighlight,
 } from 'react-native';
 import logo from '../images/logo.png';
 import styles from '../styles/home';
+import routes from '../routes';
 
-const Home = () => (
-  <View style={styles.container}>
-    <Image style={styles.logo} source={logo} />
-    <TouchableHighlight style={styles.button} onPress={this.initiateConnect}>
-      <Text style={styles.buttonText}>CONNECT</Text>
-    </TouchableHighlight>
-  </View>
-);
+const DeviceManagementService = NativeModules.DeviceManagementService;
 
-Home.propTypes = {
-  navigator: React.PropTypes.object,
-};
+export default class Initiate extends Component {
+  static propTypes = {
+    navigator: React.PropTypes.object,
+  };
 
-export default Home;
+  constructor() {
+    super();
+    this.state = {
+      connecting: false,
+    };
+
+    this.initiateConnect = this.initiateConnect.bind(this);
+  }
+
+  initiateConnect() {
+    DeviceManagementService.checkForSavedDevice((error, response) => {
+      if (error) {
+        console.log('Error: ', error);
+      } else if (!response) {
+        this.props.navigator.push(routes.devices);
+      } else {
+        this.props.navigator.push(routes.posture);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+        {this.state.connecting ?
+          (<View style={styles.connectIndicator}>
+            <ActivityIndicator
+              animating
+              size="large"
+            />
+          </View>) :
+          (<TouchableHighlight style={styles.button} onPress={this.initiateConnect}>
+            <Text style={styles.buttonText}>CONNECT</Text>
+          </TouchableHighlight>)
+        }
+      </View>
+    );
+  }
+}
