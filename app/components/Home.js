24fdfab158d9ad69e52e4ab10922794191eb7142
Khoa Phan <kp@gobackbone.com>
2016-08-11 12:27:27 -0700

    Add button to static component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   View,
+  Text,
   Image,
   TouchableHighlight,
 } from 'react-native';
@@ -10,6 +11,9 @@ import styles from '../styles/home';
 const Home = () => (
   <View style={styles.container}>
     <Image style={styles.logo} source={logo} />
+    <TouchableHighlight style={styles.button} onPress={this.initiateConnect}>
+      <Text style={styles.buttonText}>CONNECT</Text>
+    </TouchableHighlight>
   </View>
 );
 
