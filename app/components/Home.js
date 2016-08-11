94fea9ca2cb78470c6ea877e0837700909d979c6
Khoa Phan <kp@gobackbone.com>
2016-08-11 16:14:20 -0700

    Add error handling function

--- app/components/Home.js
+++ app/components/Home.js
@@ -26,6 +26,7 @@ export default class Initiate extends Component {
     };
 
     this.initiateConnect = this.initiateConnect.bind(this);
+    this.handleConnectError = this.handleConnectError.bind(this);
     this.unpairDevice = this.unpairDevice.bind(this);
   }
 
@@ -35,23 +36,28 @@ export default class Initiate extends Component {
     }, () => {
       DeviceManagementService.checkForSavedDevice((error, response) => {
         if (error) {
-          this.setState({
-            connecting: false,
-          }, () => {
-            Alert.alert(
-              'Error!',
-              'Backbone must be within range or fully charged.',
-              [
-                { text: 'Try Again' },
-                { text: 'Unpair Backbone', onPress: this.unpairDevice },
-              ]);
-          });
+          this.handleConnectError(error);
         } else if (!response) {
           this.props.navigator.push(routes.devices);
         } else {
           this.props.navigator.push(routes.posture);
         }
       });
+    });
+  }
+
+  handleConnectError(error) {
+    console.log('Error placeholder: ', error);
+    this.setState({
+      connecting: false,
+    }, () => {
+      Alert.alert(
+        'Error!',
+        'Backbone must be within range or fully charged.',
+        [
+          { text: 'Try Again' },
+          { text: 'Unpair Backbone', onPress: this.unpairDevice },
+        ]);
     });
   }
 
