28a381366e4e0ef8a385fd6d344ef94e9787a9c5
Khoa Phan <kp@gobackbone.com>
2016-08-12 12:42:19 -0700

    Minor styling changes

--- app/styles/home.js
+++ app/styles/home.js
@@ -2,19 +2,19 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
   container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   logo: {
     width: 300,
     height: 50,
-    marginTop: 100,
-    marginBottom: 100,
   },
   button: {
-    height: 75,
     width: 300,
-    borderRadius: 4,
+    height: 60,
+    marginTop: 25,
+    borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '$primaryColor',
@@ -24,18 +24,28 @@ export default EStyleSheet.create({
     fontSize: '1.5rem',
     fontWeight: 'bold',
   },
-  disabled: {
-    height: 75,
-    width: 300,
-    borderRadius: 4,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    backgroundColor: '$disabledColor',
-  },
   connectingText: {
     color: 'white',
     fontSize: '1.5rem',
     marginLeft: 10,
+  },
+  modalBodyContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.8)',
+  },
+  modalBodyContent: {
+    width: 250,
+    height: 150,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  modalBodyTitle: {
+    color: 'red',
+    fontSize: '1.5rem',
+    marginTop: 15,
   },
 });
