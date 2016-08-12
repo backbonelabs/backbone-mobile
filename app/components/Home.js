e1deb3f56ab367674210c682fb9dc6adc383a736
Khoa Phan <kp@gobackbone.com>
2016-08-12 12:35:34 -0700

    Add Modal and stateless modal body component for connecting status

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,7 +3,7 @@ import {
   View,
   Text,
   Image,
-  Alert,
+  Modal,
   NativeModules,
   ActivityIndicator,
   TouchableHighlight,
@@ -13,6 +13,21 @@ import styles from '../styles/home';
 import routes from '../routes';
 
 const DeviceManagementService = NativeModules.DeviceManagementService;
+
+const ModalBody = () => (
+  <View style={styles.modalBodyContainer}>
+    <View style={styles.modalBodyContent}>
+      <ActivityIndicator
+        animating
+        size="large"
+        color="black"
+      />
+      <Text style={styles.modalBodyTitle}>
+        Connecting
+      </Text>
+    </View>
+  </View>
+);
 
 export default class Home extends Component {
   static propTypes = {
@@ -26,61 +41,66 @@ export default class Home extends Component {
     };
 
     this.initiateConnect = this.initiateConnect.bind(this);
-    this.handleConnectError = this.handleConnectError.bind(this);
-    this.unpairDevice = this.unpairDevice.bind(this);
+    // this.handleConnectError = this.handleConnectError.bind(this);
+    // this.unpairDevice = this.unpairDevice.bind(this);
   }
 
   initiateConnect() {
     this.setState({
       connecting: true,
     }, () => {
-      DeviceManagementService.checkForSavedDevice((error, response) => {
-        if (error) {
-          this.handleConnectError(error);
-        } else if (!response) {
-          this.props.navigator.push(routes.devices);
-        } else {
-          this.props.navigator.push(routes.posture);
-        }
-      });
+      // DeviceManagementService.checkForSavedDevice((response) => {
+      //   if (response) {
+      //     Alert.alert(
+      //     'Connecting',
+      //     'Bah',
+      //     );
+      //     // this.props.navigator.push(routes.posture);
+      //   } else {
+      //     this.props.navigator.push(routes.devices);
+      //   }
+      // });
     });
   }
 
-  handleConnectError(error) {
-    this.setState({
-      connecting: false,
-    }, () => {
-      Alert.alert(
-        'Error!',
-        error.message,
-        [
-          { text: 'Try Again' },
-          { text: 'Unpair Backbone', onPress: this.unpairDevice },
-        ]);
-    });
-  }
+  // handleConnectError() {
+  //   this.setState({
+  //     connecting: false,
+  //   }, () => {
+      // Alert.alert(
+      //   'Error!',
+      //   'Error!',
+      //   [
+      //     { text: 'Try Again' },
+      //     { text: 'Unpair Backbone', onPress: this.unpairDevice },
+      //   ]);
+  //   });
+  // }
 
-  unpairDevice() {
-    DeviceManagementService.forgetDevice();
-  }
+  // unpairDevice() {
+  //   DeviceManagementService.forgetDevice();
+  // }
+
+  // {this.state.connecting ?
+  // (<View style={styles.disabled}>
+  //   <ActivityIndicator
+  //     animating
+  //     color="white"
+  //     size="large"
+  //   />
+  //   <Text style={styles.connectingText}>Connecting...</Text>
+  // </View>) :
 
   render() {
     return (
       <View style={styles.container}>
         <Image style={styles.logo} source={logo} />
-        {this.state.connecting ?
-          (<View style={styles.disabled}>
-            <ActivityIndicator
-              animating
-              color="white"
-              size="large"
-            />
-            <Text style={styles.connectingText}>Connecting...</Text>
-          </View>) :
-          (<TouchableHighlight style={styles.button} onPress={this.initiateConnect}>
-            <Text style={styles.buttonText}>Go</Text>
-          </TouchableHighlight>)
-        }
+        <TouchableHighlight style={styles.button} onPress={this.initiateConnect}>
+          <Text style={styles.buttonText}>Connect</Text>
+        </TouchableHighlight>
+        <Modal animationType="slide" visible={this.state.connecting} transparent>
+          <ModalBody />
+        </Modal>
       </View>
     );
   }
