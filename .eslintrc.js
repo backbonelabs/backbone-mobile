4948f68c5e154abe9053bb1bb207a362fbc1c52c
Khoa Phan <kp@gobackbone.com>
2016-08-13 17:35:26 -0700

    Remove 'jsx' extension from react/jsx-filename-extension rule (RN doesn't support .jsx)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   parser: 'babel-eslint',
   rules: {
-    'react/jsx-filename-extension': [1, {'extensions': ['.js', 'jsx']}],
+    'react/jsx-filename-extension': [1, {'extensions': ['.js']}],
     'max-len': [2, { 'code': 100 }],
     'consistent-return': [0],
     'no-console': [1],
