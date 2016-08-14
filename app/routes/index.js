8f67a4b509959b7cb40b058a908ade071e3af6b3
Khoa Phan <kp@gobackbone.com>
2016-08-14 12:49:50 -0700

    Import Connect route and add to route index file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import activity from './activity';
+import connect from './connect';
 import devices from './devices';
 import home from './home';
 import posture from './posture';
 
-export default { activity, devices, home, posture };
+export default { activity, connect, devices, home, posture };
