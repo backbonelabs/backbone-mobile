b4e39f08b3252be29ce84f4c0955f29437094396
Khoa Phan <kp@gobackbone.com>
2016-08-14 19:40:52 -0700

    Remove unused method declarations

--- ios/backbone/DeviceManagementService.h
+++ ios/backbone/DeviceManagementService.h
@@ -4,8 +4,6 @@
 @interface DeviceManagementService : NSObject <RCTBridgeModule>
   @property (nonatomic, strong) RCTBridge *bridge;
 + (MBLMetaWear *) getDevice;
-- (void) scanForDevices;
 - (void) checkForConnectionTimeout;
 - (void) deviceConnectionStatus :(NSDictionary *)status;
-- (void) deviceEventEmitter :(NSMutableArray *)deviceList;
 @end
