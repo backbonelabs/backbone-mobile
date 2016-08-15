5ae87005a729617ec44f95c79517e6cd7bcfe6f8
Khoa Phan <kp@gobackbone.com>
2016-08-15 16:36:23 -0700

    Basic component layout for ConnectError

---
+++ app/components/ConnectError.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from '../styles/connectError';
+import routes from '../routes/';
+
+function ConnectError(props) {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="default" />
+      <View style={styles.header}>
+        <Icon size={150} name="warning" color="black" />
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.errorCode}>
+          Error {props.currentRoute.errorCode}
+        </Text>
+        <Text style={styles.errorMessage}>
+          {props.currentRoute.errorMessage}
+        </Text>
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.retry}>Retry</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+ConnectError.propTypes = {
+  navigator: React.PropTypes.object,
+  currentRoute: React.PropTypes.object,
+  errorCode: React.PropTypes.string,
+  errorMessage: React.PropTypes.string,
+};
+
+export default ConnectError;
