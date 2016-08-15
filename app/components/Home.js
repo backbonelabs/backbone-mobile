6861c3d956e0f5413eecb85fede48efa301be63f
Khoa Phan <kp@gobackbone.com>
2016-08-14 19:55:57 -0700

    Make Home great again

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,22 +3,31 @@ import {
   View,
   Text,
   Image,
-  TouchableHighlight,
+  TouchableOpacity,
 } from 'react-native';
 import logo from '../images/logo.png';
+import bg from '../images/bg.jpg';
 import styles from '../styles/home';
 import routes from '../routes/';
 
 function Home(props) {
   return (
     <View style={styles.container}>
-      <Image style={styles.logo} source={logo} />
-      <TouchableHighlight
-        style={styles.button}
-        onPress={() => { props.navigator.push(routes.connect); }}
-      >
-        <Text style={styles.buttonText}>Connect</Text>
-      </TouchableHighlight>
+      <Image style={styles.background} source={bg} />
+      <View style={styles.header}>
+        <Image style={styles.logo} source={logo} />
+      </View>
+      <View style={styles.body}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => { props.navigator.push(routes.connect); }}
+        >
+          <Text style={styles.connect}>Connect</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.signup}>Don't have an account? Sign-up</Text>
+      </View>
     </View>
   );
 }
