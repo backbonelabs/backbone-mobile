08d3c4cae9d712d1cf669e28c528fd5bddbdd6ab
Khoa Phan <kp@gobackbone.com>
2016-08-15 16:09:58 -0700

    Add connectError route in routing index file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,8 @@
 import activity from './activity';
 import connect from './connect';
+import connectError from './connectError';
 import deviceList from './deviceList';
 import home from './home';
 import posture from './posture';
 
-export default { activity, connect, deviceList, home, posture };
+export default { activity, connect, connectError, deviceList, home, posture };
