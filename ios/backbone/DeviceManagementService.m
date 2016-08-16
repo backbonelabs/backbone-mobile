591d436a33673529b9f1f48c67d71bc6bbdda098
Khoa Phan <kp@gobackbone.com>
2016-08-16 16:14:02 -0700

    Refactor DeviceManagementService, add success/fail conditions

--- ios/backbone/DeviceManagementService.m
+++ ios/backbone/DeviceManagementService.m
@@ -22,58 +22,63 @@ static NSMutableDictionary *_deviceCollection = nil;
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(getSavedDevice :(RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(getSavedDevice:(RCTResponseSenderBlock)callback) {
   [[_manager retrieveSavedMetaWearsAsync] continueWithBlock:^id(BFTask *task) {
     if ([task.result count]) {
       NSLog(@"Found a saved device");
       _remembered = YES;
       _sharedDevice = task.result[0];
-      callback(@[@YES]);
     } else {
-      NSLog(@"No saved devices found");
+      NSLog(@"No saved device found");
       _remembered = NO;
-      callback(@[@NO]);
     }
+    callback(@[[NSNumber numberWithBool:_remembered]]);
     return nil;
   }];
 }
 
 RCT_EXPORT_METHOD(connectToDevice) {
-  NSLog(@"Attempting connection to device: %@", _sharedDevice);
   [_sharedDevice connectWithHandler:^(NSError * _Nullable error) {
     if (error) {
-      NSDictionary *makeError = RCTMakeError(@"Error", nil, @{
-                                                              @"domain": error.domain,
-                                                              @"code": [NSNumber numberWithInteger:error.code],
-                                                              @"userInfo": error.userInfo,
-                                                              @"remembered": _remembered ? @1 : @0,
-                                                              });
-      [self deviceConnectionStatus :makeError];
+      NSDictionary *makeError = RCTMakeError(@"Failed to connect to device", nil, @{
+                                                                                    @"domain": error.domain,
+                                                                                    @"code": [NSNumber numberWithLong:error.code],
+                                                                                    @"userInfo": error.userInfo,
+                                                                                    @"remembered": [NSNumber numberWithBool:_remembered],
+                                                                                    });
+      [self deviceConnectionStatus:makeError];
     } else {
-      [_sharedDevice rememberDevice];
+      if (!_remembered) {
+        [_manager stopScanForMetaWears];
+        [_sharedDevice rememberDevice];
+      }
       [_sharedDevice.led flashLEDColorAsync:[UIColor greenColor] withIntensity:1.0 numberOfFlashes:1];
-      [self deviceConnectionStatus :@{
-                                      @"message": @"Successfully connected",
-                                      @"code": [NSNumber numberWithInteger:_sharedDevice.state],
-                                      }];
+      [self deviceConnectionStatus:@{ @"message": @"Successfully connected" }];
     }
   }];
   
-  [self checkForConnectionTimeout];
+  [self checkForTimeout];
 }
 
-RCT_EXPORT_METHOD(selectDevice :(NSString *)deviceID :(RCTResponseSenderBlock)callback) {
-  [_manager stopScanForMetaWears];
+RCT_EXPORT_METHOD(selectDevice:(NSString *)deviceID:(RCTResponseSenderBlock)callback) {
   _sharedDevice = [_deviceCollection objectForKey:deviceID];
-  // Keep callback to signal _sharedDevice is set
-  callback(@[[NSNull null]]);
+  if (!_sharedDevice) {
+    NSDictionary *makeError = RCTMakeError(@"Failed to select device", nil, @{ @"remembered": [NSNumber numberWithBool:_remembered]});
+    callback(@[makeError]);
+  } else {
+    callback(@[[NSNull null]]);
+  }
 }
 
-RCT_EXPORT_METHOD(scanForDevices :(RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(scanForDevices:(RCTResponseSenderBlock)callback) {
   NSLog(@"Scanning for devices");
   _deviceCollection = [NSMutableDictionary new];
+  NSMutableArray *deviceList = [NSMutableArray new];
+  
   [_manager startScanForMetaWearsWithHandler:^(NSArray *array) {
-    NSMutableArray *deviceList = [NSMutableArray new];
+    if ([deviceList count]) {
+      [deviceList removeAllObjects];
+    }
     
     for (MBLMetaWear *device in array) {
       _deviceCollection[[device.identifier UUIDString]] = device;
@@ -83,40 +88,42 @@ RCT_EXPORT_METHOD(scanForDevices :(RCTResponseSenderBlock)callback) {
                                @"RSSI": device.discoveryTimeRSSI,
                                }];
     }
-    [_manager stopScanForMetaWears];
-    callback(@[deviceList]);
+    [self devicesFound:deviceList];
   }];
 }
 
-RCT_EXPORT_METHOD(getDeviceStatus :(RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(getDeviceStatus:(RCTResponseSenderBlock)callback) {
   callback(@[[NSNumber numberWithInteger:_sharedDevice.state]]);
 }
 
-RCT_EXPORT_METHOD(forgetDevice :(RCTResponseSenderBlock)callback) {
-  NSLog(@"Forget device %@", _sharedDevice);
+RCT_EXPORT_METHOD(forgetDevice:(RCTResponseSenderBlock)callback) {
   [_sharedDevice forgetDevice];
   _sharedDevice = nil;
-  callback(@[[NSNull null]]);
+  if (_sharedDevice) {
+    NSDictionary *makeError = RCTMakeError(@"Failed to forget device", nil, @{ @"remembered": [NSNumber numberWithBool:_remembered]});
+    callback(@[makeError]);
+  } else {
+    callback(@[[NSNull null]]);
+  }
 }
 
-- (void)checkForConnectionTimeout {
+- (void)checkForTimeout {
   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 10 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
     if (_sharedDevice.state != MBLConnectionStateConnected) {
-      NSLog(@"Device connection timed out");
-      NSDictionary *makeError = RCTMakeError(@"This device is taking too long to connect.", nil, @{
-                                                              @"domain": [NSNull null],
-                                                              @"code": [NSNull null],
-                                                              @"userInfo": [NSNull null],
-                                                              @"remembered": _remembered ? @1 : @0,
-                                                              });
-      [self deviceConnectionStatus: makeError];
+      NSDictionary *makeError = RCTMakeError(@"Device took too long to connect", nil, @{ @"remembered": [NSNumber numberWithBool:_remembered]});
+      [self deviceConnectionStatus:makeError];
+    } else {
+      [self deviceConnectionStatus:@{ @"message": @"Successfully connected" }];
     }
   });
 }
 
-- (void)deviceConnectionStatus :(NSDictionary *)status {
+- (void)deviceConnectionStatus:(NSDictionary *)status {
   [self.bridge.eventDispatcher sendAppEventWithName:@"ConnectionStatus" body: status];
 }
 
+- (void)devicesFound:(NSMutableArray *)deviceList {
+  [self.bridge.eventDispatcher sendAppEventWithName:@"DevicesFound" body: deviceList];
+}
 
 @end;
