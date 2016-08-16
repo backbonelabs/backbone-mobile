f0612e2f0c41321b31ce5e010dfbc78d72b5606f
Khoa Phan <kp@gobackbone.com>
2016-08-15 18:17:53 -0700

    Update routing index file to reflect removal of deviceList

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,7 @@
 import activity from './activity';
 import connect from './connect';
 import connectError from './connectError';
-import deviceList from './deviceList';
 import home from './home';
 import posture from './posture';
 
-export default { activity, connect, connectError, deviceList, home, posture };
+export default { activity, connect, connectError, home, posture };
