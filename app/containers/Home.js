965eb1633d1012225a6abad908dffc83f130aded
Khoa Phan <kp@gobackbone.com>
2016-08-23 19:22:52 -0700

    Move signup button/text inside ternary - displays only if no valid
    ... accessToken

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -107,10 +107,7 @@ class Home extends Component {
           }
         </View>
         <View style={styles.footer}>
-          <TouchableHighlight onPress={() => { this.props.navigator.push(routes.signup); }}>
-            <Text style={styles.signup}>Don't have an account? Sign-up</Text>
-          </TouchableHighlight>
-          {this.state.accessToken && this.props.auth.isValidAccessToken &&
+          {this.state.accessToken && this.props.auth.isValidAccessToken ?
             // This conditional block for deleting the access token is for temporary
             // testing purposes only. Remove this entire conditional block after
             // implementing a logout component.
@@ -120,7 +117,10 @@ class Home extends Component {
               }}
             >
               <Text style={{ fontSize: 18, color: 'white' }}>Delete access token</Text>
-            </TouchableOpacity>
+            </TouchableOpacity> :
+            <TouchableHighlight onPress={() => { this.props.navigator.push(routes.signup); }}>
+              <Text style={styles.signup}>Don't have an account? Sign-up</Text>
+            </TouchableHighlight>
           }
         </View>
       </View>
