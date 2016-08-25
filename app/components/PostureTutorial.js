2c54f079c103973aaaf59b80756b6da506b246c0
Khoa Phan <kp@gobackbone.com>
2016-08-25 13:46:44 -0700

    Create `PostureTutorial` sub-scene files (component and style)

---
+++ app/components/PostureTutorial.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import styles from '../styles/postureTutorial';
+
+const PostureTutorial = (props) => (
+  <View>
+    <Text>Posture Tutorial</Text>
+  </View>
+);
+
+export default PostureTutorial;
