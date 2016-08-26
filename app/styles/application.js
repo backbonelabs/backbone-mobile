b1c1a0db1ffc888be50639a1598f48fdea085f1e
Khoa Phan <kp@gobackbone.com>
2016-08-25 17:53:42 -0700

    Modify nav bar styling

--- app/styles/application.js
+++ app/styles/application.js
@@ -4,32 +4,37 @@ export default EStyleSheet.create({
   navbarContainer: {
     flex: 1,
     width: '100%',
-    height: '$iconButtonSize',
-    marginTop: '1%',
+    height: '7%',
+    marginTop: '2%',
     flexDirection: 'row',
   },
+  navBarTitle: {
+    flex: 0.3,
+  },
+  navBarText: {
+    fontSize: '1.25rem',
+    textAlign: 'center',
+  },
   menuContainer: {
-    flex: 0.5,
+    flex: 0.35,
     alignSelf: 'flex-start',
   },
   menuButton: {
     marginLeft: '2%',
     width: '$iconButtonSize',
     height: '$iconButtonSize',
-    justifyContent: 'center',
   },
   menuIcon: {
     alignSelf: 'center',
   },
   settingsContainer: {
-    flex: 0.5,
+    flex: 0.35,
     alignItems: 'flex-end',
   },
   settingsButton: {
     marginRight: '2%',
     width: '$iconButtonSize',
     height: '$iconButtonSize',
-    justifyContent: 'center',
   },
   settingsIcon: {
     alignSelf: 'center',
