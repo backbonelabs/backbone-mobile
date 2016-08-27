52225a566912e306c3478dda09f8ac237da6b496
Kevin Huang <kev@gobackbone.com>
2016-08-26 19:55:43 -0700

    Add utility helper for creating a JS-compatible map

---
+++ android/app/src/main/java/co/backbonelabs/Backbone/util/JSError.java
@@ -0,0 +1,27 @@
+package co.backbonelabs.Backbone.util;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class JSError {
+    public static WritableMap make(String message) {
+        WritableMap error = Arguments.createMap();
+        error.putString("message", message);
+        return error;
+    }
+
+    public static WritableMap make(String message, HashMap<String, String> details) {
+        WritableMap error = Arguments.createMap();
+        Iterator<Map.Entry<String, String>> iterator = details.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> pair = iterator.next();
+            error.putString(pair.getKey(), pair.getValue());
+        }
+        error.putString("message", message);
+        return error;
+    }
+}
