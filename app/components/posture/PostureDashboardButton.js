ba30138b15c96fb052e40ca4fa49076ff7d97d8e
Khoa Phan <kp@gobackbone.com>
2016-08-27 18:04:42 -0700

    Remove block statement surrounding arrow body

--- app/components/posture/PostureDashboardButton.js
+++ app/components/posture/PostureDashboardButton.js
@@ -6,19 +6,18 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import postureRoutes from '../../routes/posture';
 import styles from '../../styles/posture/postureDashboard';
 
-const PostureDashboardButton = (props) => {
-  return (
-    <TouchableOpacity
-      style={props.navStyle}
-      onPress={() => props.navigator.push(postureRoutes.postureTutorial)}
-    >
-      <Icon
-        name="question-circle-o"
-        size={props.iconSize}
-        color={styles._button.color}
-      />
-    </TouchableOpacity>);
-};
+const PostureDashboardButton = (props) => (
+  <TouchableOpacity
+    style={props.navStyle}
+    onPress={() => props.navigator.push(postureRoutes.postureTutorial)}
+  >
+    <Icon
+      name="question-circle-o"
+      size={props.iconSize}
+      color={styles._button.color}
+    />
+  </TouchableOpacity>
+);
 
 PostureDashboardButton.propTypes = {
   iconSize: React.PropTypes.number,
