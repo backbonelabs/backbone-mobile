8e3a15c8dc5e5c28bf8cd3e069733a3c48f7c42e
Kevin Huang <kev@gobackbone.com>
2016-08-31 16:02:41 -0700

    Add Spinner component

---
+++ app/components/Spinner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  View,
+} from 'react-native';
+import styles from '../styles/spinner';
+
+const Spinner = props => (
+  <View style={styles.container}>
+    <ActivityIndicator color={styles._spinner.color} {...props} />
+  </View>
+);
+
+const { PropTypes } = React;
+Spinner.propTypes = {
+  animating: PropTypes.bool,
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([
+    PropTypes.oneOf(['small', 'large']),
+    PropTypes.number,
+  ]),
+};
+
+Spinner.defaultProps = {
+  animating: true,
+  size: 'large',
+};
+
+export default Spinner;
