e5dff0c080dd1de44ef560f37ff24bf91594325c
Kevin Huang <kev@gobackbone.com>
2016-08-30 19:56:15 -0700

    Add action creators for retrieving and updating user profile

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -1,17 +1,40 @@
 export default (state = {
+  isFetching: false,
   isUpdating: false,
   user: null,
   errorMessage: null,
 }, action) => {
   switch (action.type) {
-    case 'UPDATE_PROFILE__START': {
+    case 'FETCH_USER__START': {
+      return {
+        ...state,
+        isFetching: true,
+        errorMessage: null,
+      };
+    }
+    case 'FETCH_USER': {
+      return {
+        ...state,
+        isFetching: false,
+        user: action.payload,
+        errorMessage: null,
+      };
+    }
+    case 'FETCH_USER__ERROR': {
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload.message,
+      };
+    }
+    case 'UPDATE_USER__START': {
       return {
         ...state,
         isUpdating: true,
         errorMessage: null,
       };
     }
-    case 'UPDATE_PROFILE': {
+    case 'UPDATE_USER': {
       return {
         ...state,
         isUpdating: false,
@@ -19,7 +42,7 @@ export default (state = {
         user: action.payload,
       };
     }
-    case 'UPDATE_PROFILE__ERROR': {
+    case 'UPDATE_USER__ERROR': {
       return {
         ...state,
         isUpdating: false,
