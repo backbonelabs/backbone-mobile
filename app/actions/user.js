290b0aef76687063a9cfa071597760ac27a53555
Khoa Phan <kp@gobackbone.com>
2016-09-01 23:15:51 -0700

    Create functions for fetching and updating user's settings

--- app/actions/user.js
+++ app/actions/user.js
@@ -3,6 +3,7 @@ import Fetcher from '../utils/Fetcher';
 
 const { Environment } = NativeModules;
 const baseUrl = `${Environment.API_SERVER_URL}/users`;
+const settingsUrl = `${Environment.API_SERVER_URL}/users/settings`;
 
 const fetchUserStart = () => ({ type: 'FETCH_USER__START' });
 
@@ -26,6 +27,32 @@ const updateUser = payload => ({
 
 const updateUserError = error => ({
   type: 'UPDATE_USER__ERROR',
+  payload: error,
+  error: true,
+});
+
+const fetchUserSettingsStart = () => ({ type: 'FETCH_USER_SETTINGS__START' });
+
+const fetchUserSettings = payload => ({
+  type: 'FETCH_USER_SETTINGS',
+  payload,
+});
+
+const fetchUserSettingsError = error => ({
+  type: 'FETCH_USER_SETTINGS__ERROR',
+  payload: error,
+  error: true,
+});
+
+const updateUserSettingsStart = () => ({ type: 'UPDATE_USER_SETTINGS__START' });
+
+const updateUserSettings = payload => ({
+  type: 'UPDATE_USER_SETTINGS',
+  payload,
+});
+
+const updateUserSettingsError = error => ({
+  type: 'UPDATE_USER_SETTINGS__ERROR',
   payload: error,
   error: true,
 });
@@ -94,6 +121,78 @@ export default {
         .catch(() => {
           // Network error
           dispatch(updateUserError(
+            new Error('We are encountering server issues. Please try again later.')
+          ));
+        });
+    };
+  },
+
+  fetchUserSettings() {
+    return (dispatch, getState) => {
+      const state = getState();
+      const { accessToken, userId } = state.auth;
+
+      dispatch(fetchUserSettingsStart());
+
+      return Fetcher.get({
+        url: `${settingsUrl}/${userId}`,
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+        .then(response => response.json()
+          .then(body => {
+            console.log('body is ', body);
+            if (body.error) {
+              // Error received from API server
+              dispatch(fetchUserSettingsError(
+                new Error(body.error)
+              ));
+            } else {
+              dispatch(fetchUserSettings(body));
+            }
+          })
+        )
+        .catch(() => {
+          // Network error
+          dispatch(fetchUserSettingsError(
+            new Error('We are encountering server issues. Please try again later.')
+          ));
+        });
+    };
+  },
+
+  updateUserSettings(user) {
+    const {
+      _id,
+      ...userSettingsUpdateFields,
+    } = user;
+
+    return (dispatch, getState) => {
+      const state = getState();
+      const { accessToken } = state.auth;
+
+      dispatch(updateUserSettingsStart());
+
+      return Fetcher.post({
+        url: `${settingsUrl}/${_id}`,
+        headers: { Authorization: `Bearer ${accessToken}` },
+        body: JSON.stringify(userSettingsUpdateFields),
+      })
+        .then(response => response.json()
+          .then(body => {
+            console.log('body ', body);
+            if (body.error) {
+              // Error received from API server
+              dispatch(updateUserSettingsError(
+                new Error(body.error)
+              ));
+            } else {
+              dispatch(updateUserSettings(body));
+            }
+          })
+        )
+        .catch(() => {
+          // Network error
+          dispatch(updateUserSettingsError(
             new Error('We are encountering server issues. Please try again later.')
           ));
         });
