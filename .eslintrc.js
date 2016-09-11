754c957383e8c0b18f48f8cbc757c47e75b2de3a
Khoa Phan <kp@gobackbone.com>
2016-09-11 16:52:42 -0700

    New rules added by eslint-config-airbnb - reduced to warnings for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,16 @@ module.exports = {
     'no-console': [1],
     'no-underscore-dangle': [0],
     'prefer-template': [1],
+
+    // New rules added by latest eslint-config-airbnb
+
+    // Functions that don't use `this` have to be static
+    'class-methods-use-this': [1],
+
+    // Not sure how to use with "vague" props like style
+    'react/forbid-prop-types': [1],
+
+    // Causes issues with prop destructuring assignment
+    'react/no-unused-prop-types': [1],
   },
 };
