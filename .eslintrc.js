cf52eb521291e5e4a8ddbfcf835c06e2335f3f47
Khoa Phan <kp@gobackbone.com>
2016-09-11 17:29:02 -0700

    Max len error

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,11 @@ module.exports = {
   },
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
-    'max-len': [2, { code: 100 }],
     'consistent-return': [0],
     'no-console': [1],
     'no-underscore-dangle': [0],
     'prefer-template': [1],
+    'max-len': [2, {"code": 100}],
 
     // New rules added by latest eslint-config-airbnb
 
