fff29fa096856d22c2a11deae5855f00493e530b
Khoa Phan <kp@gobackbone.com>
2016-09-13 16:30:31 -0700

    Add route and style files for `Device` container

---
+++ app/styles/device.js
@@ -0,0 +1,19 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: '$totalNavHeight',
+  },
+  progress: {
+    flex: 1,
+  },
+  deviceName: {
+    color: 'white',
+    fontSize: '1.25rem',
+  },
+  deviceID: {
+    marginTop: '0.5rem',
+    fontSize: '0.5rem',
+  },
+});
