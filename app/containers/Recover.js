b940fab9844860cc2285c0f5f0dd38cd712a700c
Khoa Phan <kp@gobackbone.com>
2016-09-15 10:52:02 -0700

    Create `Recover` scene, which allows the user to enter in their email
    ... and request a password change

---
+++ app/containers/Recover.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import {
+  View,
+} from 'react-native';
+import { connect } from 'react-redux';
+import authActions from '../actions/auth';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import styles from '../styles/auth';
+
+class Recover extends Component {
+  static propTypes = {
+    dispatch: React.PropTypes.func,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      email: null,
+    };
+    this.recoverPassword = this.recoverPassword.bind(this);
+  }
+
+  recoverPassword() {
+    this.props.dispatch(authActions.recover(this.state.email));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.formContainer}>
+          <Input
+            autoCapitalize="none"
+            placeholder="Email"
+            keyboardType="email-address"
+            onChangeText={text => this.setState({ email: text })}
+            onSubmitEditing={() => this.recoverPassword}
+            autoCorrect={false}
+            autoFocus
+            returnKeyType="go"
+          />
+          <Button text="Recover" onPress={this.recoverPassword} />
+        </View>
+      </View>
+    );
+  }
+}
+
+export default connect()(Recover);
---
+++ app/routes/recover.js
@@ -0,0 +1,8 @@
+import Recover from '../containers/Recover';
+
+export default {
+  name: 'recover',
+  title: 'Recover Password',
+  component: Recover,
+  showBackButton: true,
+};
