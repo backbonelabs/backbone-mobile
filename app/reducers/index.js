45d58a3fdf4b6d0195ce82cd4a02424111ce4d80
Khoa Phan <kp@gobackbone.com>
2016-09-18 20:36:12 -0700

    Reflect file name change in reducer index file

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import user from './user';
-import generic from './generic';
+import device from './device';
 
 export default combineReducers({
   auth,
   user,
-  generic,
+  device,
 });
