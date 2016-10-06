4b964d3daa9d9b174ad4df2d7219a1504e407f65
Kevin Huang <kev@gobackbone.com>
2016-10-06 11:46:55 -0700

    Store app config

--- app/reducers/generic.js
+++ app/reducers/generic.js
@@ -1,11 +1,33 @@
+import { isString, mapValues } from 'lodash';
+
 export default (state = {
   bluetoothState: null,
+  config: {},
 }, action) => {
   switch (action.type) {
     case 'UPDATE_BLUETOOTH_STATE': {
       return {
         ...state,
         bluetoothState: action.payload,
+      };
+    }
+    case 'SET_CONFIG': {
+      // Convert string true/false values to primitive boolean
+      const transformedConfig = mapValues(action.payload, value => {
+        if (isString(value)) {
+          const _value = value.toLowerCase();
+          if (_value === 'true') {
+            return true;
+          } else if (_value === 'false') {
+            return false;
+          }
+        }
+        return value;
+      });
+
+      return {
+        ...state,
+        config: transformedConfig,
       };
     }
     default:
