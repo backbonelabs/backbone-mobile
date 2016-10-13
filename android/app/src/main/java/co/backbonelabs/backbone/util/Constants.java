2b51127dd23af155014331ab445e0c6c00ee15e2
Kevin Huang <kev@gobackbone.com>
2016-10-13 10:22:58 -0700

    Keep posture activity running after tapping the notification
    Before, tapping the notification from the posture foreground service would disable
    the posture activity module because `SensorDataService` issues the
    `STOP_POSTURE_FOREGROUND_SERVICE` intent in `onActivityResumed`, and the foreground
    service had logic to kill the posture activity when receiving the
    `STOP_POSTURE_FOREGROUND_SERVICE` intent. This change updates the logic for
    `STOP_POSTURE_FOREGROUND_SERVICE` so that it will only stop the foreground service
    and not kill the posture activity.
    
    A new `STOP_POSTURE_ACTIVITY` is added and would be used to explicitly kill the
    posture activity.

--- android/app/src/main/java/co/backbonelabs/backbone/util/Constants.java
+++ android/app/src/main/java/co/backbonelabs/backbone/util/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public interface ACTIONS {
         String START_POSTURE_FOREGROUND_SERVICE = "co.backbonelabs.backbone.action.startPostureForegroundService";
         String STOP_POSTURE_FOREGROUND_SERVICE = "co.backbonelabs.backbone.action.stopPostureForegroundService";
+        String STOP_POSTURE_ACTIVITY = "co.backbonelabs.backbone.action.stopPostureActivity";
     }
     public interface NOTIFICATION_IDS {
         int FOREGROUND_SERVICE = 101;
