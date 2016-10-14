0c5b5783a082a1b3435f33c55ca6cfa059c3b041
Khoa Phan <kp@gobackbone.com>
2016-10-13 23:05:57 -0700

    Manage device connect state in Redux

--- app/actions/app.js
+++ app/actions/app.js
@@ -1,8 +1,44 @@
+import { NativeModules, NativeAppEventEmitter } from 'react-native';
+
+const { DeviceManagementService } = NativeModules;
+
+const connectStart = () => ({ type: 'CONNECT__START' });
+
+const connect = payload => ({
+  type: 'CONNECT',
+  payload,
+});
+
+const connectError = error => ({
+  type: 'CONNECT__ERROR',
+  error,
+});
+
+async function connectEventListener(dispatch) {
+  const eventListener = NativeAppEventEmitter.once('ConnectionStatus', status => {
+    if (status.isConnected) {
+      dispatch(connect(status));
+    } else {
+      dispatch(connectError(status));
+    }
+  });
+
+  return eventListener;
+}
+
 export default {
   setConfig(config) {
     return {
       type: 'SET_CONFIG',
       payload: config,
+    };
+  },
+  connect() {
+    return (dispatch) => {
+      dispatch(connectStart());
+
+      return connectEventListener(dispatch)
+      .then(DeviceManagementService.connectToDevice());
     };
   },
 };
