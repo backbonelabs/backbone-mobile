a6856c10b37ba53da7f463bb246e878e2be1065f
Khoa Phan <kp@gobackbone.com>
2016-10-25 18:59:32 -0700

    Removing button global styling variables, unnecessary:
    Will create reusable buttons + their specific styling files

--- app/styles/theme.js
+++ app/styles/theme.js
@@ -9,18 +9,12 @@ const primaryColor = '#ED1C24';
 const primaryFont = 'Lato';
 const primaryFontColor = '#231F20';
 const secondaryFontColor = '#A9A9A9';
-const disabledPrimaryFontColor = 'rgba(255, 255, 255, 0.4)';
-const disabledSecondaryFontColor = 'rgba(237, 29, 35, 0.4)';
-const pressedButtonFontColor = 'rgba(134, 25, 28, 0.4)';
 
 export default {
   primaryColor,
   primaryFont,
   primaryFontColor,
   secondaryFontColor,
-  disabledPrimaryFontColor,
-  disabledSecondaryFontColor,
-  pressedButtonFontColor,
   disabledColor: '#ccc',
   iconSize,
   iconButtonSize,
