c956fab949ea37dbecbdfdbeed5e950a51c0249b
Miguel Correa <miguelc1221@gmail.com>
2016-10-26 09:44:09 -0700

    reusable button component

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,40 +1,100 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
-  TouchableOpacity,
+  View,
+  TouchableHighlight,
 } from 'react-native';
 import BodyText from './BodyText';
 import styles from '../styles/button';
 
-const Button = (props) => {
-  const buttonStyles = [styles._button];
-  const textStyles = [styles._text];
-  if (props.disabled) {
-    buttonStyles.push(styles._disabledButton);
-    textStyles.push(styles._disabledText);
+
+class Button extends Component {
+
+  static propTypes = {
+    disabled: PropTypes.bool,
+    onPress: PropTypes.func,
+    style: PropTypes.object,
+    text: PropTypes.string.isRequired,
+    textStyle: PropTypes.object,
+    buttonType: PropTypes.string,
+    pressStatus: PropTypes.bool,
+    onHideUnderlay: PropTypes.func,
+    onShowUnderlay: PropTypes.func,
   }
-  buttonStyles.push(props.style);
-  textStyles.push(props.textStyle);
 
-  return (
-    <TouchableOpacity style={buttonStyles} onPress={props.disabled ? undefined : props.onPress}>
-      <BodyText style={textStyles}>{props.text}</BodyText>
-    </TouchableOpacity>
-  );
-};
+  static defaultProps = {
+    style: {},
+    textStyle: {},
+  };
 
-const { PropTypes } = React;
+  constructor(props) {
+    super(props);
+    this.state = {
+      pressStatus: false,
+    };
 
-Button.propTypes = {
-  disabled: PropTypes.bool,
-  onPress: PropTypes.func,
-  style: PropTypes.object,
-  text: PropTypes.string.isRequired,
-  textStyle: PropTypes.object,
-};
+    this._onHideUnderlay = this._onHideUnderlay.bind(this);
+    this._onShowUnderlay = this._onShowUnderlay.bind(this);
+  }
 
-Button.defaultProps = {
-  style: {},
-  textStyle: {},
-};
+  _onHideUnderlay() {
+    this.setState({ pressStatus: false });
+  }
+  _onShowUnderlay() {
+    this.setState({ pressStatus: true });
+  }
+
+  render() {
+    let buttonType;
+    const textStyles = [styles._text];
+    const buttonStyles = [styles._button];
+    const buttonActive = [buttonStyles, styles._buttonActive];
+    const secondaryStyles = [buttonStyles, styles._secondaryBtn];
+    const secondaryActive = [buttonStyles, styles._secondaryActive];
+    const secondaryTextStyles = [styles._text, styles._secondaryTextStyles];
+
+    if (this.props.buttonType === 'mainCtsBtn') {
+      buttonType = (
+        <TouchableHighlight
+          activeOpacity={.4}
+          style={this.state.pressStatus ? buttonActive : buttonStyles}
+          onHideUnderlay={this._onHideUnderlay}
+          onShowUnderlay={this._onShowUnderlay}
+          onPress={this.props.disabled ? undefined : this.props.onPress}
+        >
+          <BodyText style={textStyles}>{this.props.text}</BodyText>
+        </TouchableHighlight>
+      );
+    } else if (this.props.buttonType === 'secondaryBtn') {
+      buttonType = (
+        <TouchableHighlight
+          activeOpacity={.4}
+          style={this.state.pressStatus ? secondaryActive : secondaryStyles}
+          onHideUnderlay={this._onHideUnderlay}
+          onShowUnderlay={this._onShowUnderlay}
+          onPress={this.props.disabled ? undefined : this.props.onPress}
+        >
+          <BodyText style={secondaryTextStyles}>{this.props.text}</BodyText>
+        </TouchableHighlight>
+      );
+    } else {
+      buttonType = null;
+    }
+
+    if (this.props.disabled) {
+      buttonStyles.push(styles._disabledButton);
+      textStyles.push(styles._disabledText);
+      secondaryStyles.push(styles._disabledSecondaryBorder);
+      secondaryTextStyles.push(styles._disabledSecondaryText);
+    }
+    buttonStyles.push(this.props.style);
+    textStyles.push(this.props.textStyle);
+
+    return (
+      <View>
+        { buttonType }
+      </View>
+    );
+  }
+}
 
 export default Button;
--- app/styles/button.js
+++ app/styles/button.js
@@ -8,11 +8,45 @@ export default EStyleSheet.create({
     backgroundColor: '$primaryColor',
     minWidth: 150,
     maxWidth: 225,
+    width: 150,
     height: 40,
+    paddingTop: 10,
+    paddingRight: 20,
+    paddingBottom: 11,
+    paddingLeft: 20,
     borderRadius: 5,
+  },
+  buttonActive: {
+    backgroundColor: '#85181C',
+  },
+  secondaryBtn: {
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderColor: '$primaryColor',
+  },
+  secondaryActive: {
+    borderColor: '#85181C',
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+  },
+  secondaryTextStyles: {
+    color: '#ED1C24',
+  },
+  secondaryTextActive: {
+    color: '#85181C',
   },
   disabledButton: {
     backgroundColor: () => (
+      color(EStyleSheet.value('$primaryColor')).clearer(0.6).rgbString() // 40% opacity
+    ),
+  },
+  disabledSecondaryText: {
+    color: () => (
+      color(EStyleSheet.value('$primaryColor')).clearer(0.6).rgbString() // 40% opacity
+    ),
+  },
+  disabledSecondaryBorder: {
+    borderColor: () => (
       color(EStyleSheet.value('$primaryColor')).clearer(0.6).rgbString() // 40% opacity
     ),
   },
