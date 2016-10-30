ca5d001cd59c7789871d18db8b9e7aef3d7c0406
Kevin Huang <kev@gobackbone.com>
2016-10-30 15:56:00 -0700

    Convert to stateless component

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import {
   View,
   Image,
@@ -17,7 +17,6 @@ import Icon20Min from '../images/session/20min.png';
 import IconUnlimited from '../images/session/unlimited.png';
 import DailyStreakBanner from '../images/session/dailyStreakBanner.png';
 
-const { PropTypes } = React;
 const sessions = [
   { id: '5min', icon: Icon5Min },
   { id: '10min', icon: Icon10Min },
@@ -26,64 +25,53 @@ const sessions = [
   { id: 'unlimited', icon: IconUnlimited },
 ];
 
-class Home extends Component {
-  static propTypes = {
-    navigator: PropTypes.shape({
-      push: PropTypes.func,
-    }),
-    user: PropTypes.object,
-  };
+const renderItem = (session) => (
+  <View key={session.id}>
+    <Image source={session.icon} />
+  </View>
+);
 
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  renderItem(session) {
-    return (
-      <View key={session.id}>
-        <Image source={session.icon} />
+const Home = (props) => (
+  <View style={styles.container}>
+    <View style={styles.header}>
+      <HeadingText size={2}>{props.user.firstName}</HeadingText>
+      <HeadingText size={2}>Choose your goal</HeadingText>
+    </View>
+    <View style={styles.body}>
+      <View style={styles.carouselContainer}>
+        <Carousel
+          items={sessions}
+          renderItem={renderItem}
+          snapOnAndroid
+          sliderWidth={styles.$sliderWidth}
+          itemWidth={styles.$itemWidth}
+          slideStyle={styles.carouselItem}
+          inactiveSlideScale={0.8}
+        />
       </View>
-    );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <HeadingText size={2}>{this.props.user.firstName}</HeadingText>
-          <HeadingText size={2}>Choose your goal</HeadingText>
-        </View>
-        <View style={styles.body}>
-          <View style={styles.carouselContainer}>
-            <Carousel
-              items={sessions}
-              renderItem={this.renderItem}
-              snapOnAndroid
-              sliderWidth={styles.$sliderWidth}
-              itemWidth={styles.$itemWidth}
-              slideStyle={styles.carouselItem}
-              inactiveSlideScale={0.8}
-            />
-          </View>
-          <Button
-            text="START"
-            primary
-            style={styles._startButton}
-            onPress={() => this.props.navigator.push(routes.postureDashboard)}
-          />
-        </View>
-        <View style={styles.footer}>
-          <BodyText>DAILY STREAK</BodyText>
-          <View style={styles.dailyStreakContainer}>
-            <Image source={DailyStreakBanner} style={styles.dailyStreakBanner} />
-            <BodyText style={styles._streakCounter}>{this.props.user.dailyStreak || 0}</BodyText>
-          </View>
-        </View>
+      <Button
+        text="START"
+        primary
+        style={styles._startButton}
+        onPress={() => props.navigator.push(routes.postureDashboard)}
+      />
+    </View>
+    <View style={styles.footer}>
+      <BodyText>DAILY STREAK</BodyText>
+      <View style={styles.dailyStreakContainer}>
+        <Image source={DailyStreakBanner} style={styles.dailyStreakBanner} />
+        <BodyText style={styles._streakCounter}>{props.user.dailyStreak || 0}</BodyText>
       </View>
-    );
-  }
-}
+    </View>
+  </View>
+);
+
+Home.propTypes = {
+  navigator: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+  user: PropTypes.object,
+};
 
 const mapStateToProps = (state) => {
   const { user: { user } } = state;
