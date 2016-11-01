0321e7599773b78186d0bf6741473dacfede2426
Kevin Huang <kev@gobackbone.com>
2016-11-01 16:41:34 -0700

    Fix styles

--- app/styles/welcome.js
+++ app/styles/welcome.js
@@ -1,18 +1,26 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
+import relativeDimensions from '../utils/relativeDimensions';
+
+const { widthDifference, heightDifference } = relativeDimensions;
+const width = 330 * widthDifference;
 
 export default EStyleSheet.create({
-  $containerMargin: 22,
-  $edgeMargins: '$containerMargin * 2',
+  $containerMarginPt: 20,
+  $heightDifference: heightDifference,
+  $widthDifference: widthDifference,
+  $topMargin: '$containerMarginPt * $heightDifference',
   container: {
     flex: 1,
-    marginHorizontal: '$containerMargin',
-    marginTop: '$statusBarHeight + $containerMargin',
-    marginBottom: '$containerMargin',
+    marginTop: '$statusBarHeight + $topMargin',
+    marginBottom: '$containerMarginPt * $heightDifference',
+    alignItems: 'center',
+    alignSelf: 'center',
   },
   body: {
     flex: 0.8,
     alignItems: 'center',
     justifyContent: 'center',
+    width,
   },
   heading: {
     marginTop: 30,
@@ -27,10 +35,12 @@ export default EStyleSheet.create({
     flex: 0.2,
     alignItems: 'center',
     justifyContent: 'flex-end',
+    width,
   },
   CTAContainer: {
-    width: '$screenWidth - $edgeMargins',
     flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'space-around',
+    width,
   },
 });
