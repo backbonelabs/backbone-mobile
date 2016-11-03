ddaf7d9b8aef24adaecc6ea40a75f13fb6ac4ecf
Kevin Huang <kev@gobackbone.com>
2016-11-03 02:11:02 -0700

    Create custom TitleBar component

---
+++ app/styles/titleBar.js
@@ -0,0 +1,43 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+import theme from '../styles/theme';
+import relativeDimensions from '../utils/relativeDimensions';
+
+const baseTitleBarStyles = {
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: theme.primaryColor,
+  paddingTop: theme.statusBarHeight,
+};
+
+export default EStyleSheet.create({
+  $leftButtonIconSize: 28 * relativeDimensions.heightDifference,
+  visibleTitleBar: {
+    ...baseTitleBarStyles,
+    minHeight: 64 * relativeDimensions.heightDifference,
+  },
+  hiddenTitleBar: {
+    ...baseTitleBarStyles,
+    height: 0,
+  },
+  title: {
+    flex: 0.34,
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  sideContainers: {
+    flex: 0.33,
+    paddingHorizontal: 6 * relativeDimensions.widthDifference,
+  },
+  sideContainersText: {
+    color: '#FFFFFF',
+  },
+  sideButtons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonIcon: {
+    color: '#FFFFFF',
+    marginRight: 6 * relativeDimensions.widthDifference,
+  },
+});
