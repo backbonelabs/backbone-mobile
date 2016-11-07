0003d392214c15c05df1230207719c3434bb2478
Miguel Correa <miguelc1221@gmail.com>
2016-11-07 10:34:51 -0800

    centered spinner

--- app/components/Spinner.js
+++ app/components/Spinner.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { ActivityIndicator } from 'react-native';
 import styles from '../styles/spinner';
 
-const Spinner = props => <ActivityIndicator color={styles._spinner.color} {...props} />;
+const Spinner = props => (
+  <ActivityIndicator style={styles.spinnerPosition} color={styles._spinner.color} {...props} />
+);
 
-const { PropTypes } = React;
 Spinner.propTypes = {
   animating: PropTypes.bool,
   color: PropTypes.string,
