6e0b4c930205804f29cabfb09569386f515791ed
Kevin Huang <huang.kev@gmail.com>
2016-11-06 17:04:27 -0800

    Use relative dimensions

--- app/styles/input.js
+++ app/styles/input.js
@@ -1,4 +1,5 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
+import relativeDimensions from '../utils/relativeDimensions';
 
 export default EStyleSheet.create({
   $placeholderTextColor: '#A9A9A9',
@@ -13,14 +14,14 @@ export default EStyleSheet.create({
     borderColor: '#979797',
     borderWidth: 1,
     borderRadius: 5,
-    width: 235,
-    height: 39,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
+    width: 235 * relativeDimensions.widthDifference,
+    height: 39 * relativeDimensions.heightDifference,
+    paddingHorizontal: 10 * relativeDimensions.widthDifference,
+    paddingVertical: 10 * relativeDimensions.heightDifference,
   },
   icon: {
     position: 'relative',
-    right: 20,
+    right: 20 * relativeDimensions.widthDifference,
   },
   disabled: {
     color: '$disabledColor',
