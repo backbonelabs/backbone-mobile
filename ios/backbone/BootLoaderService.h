68738ea09c202d2df04a822bbca802399eb33d5e
Eko Mirhard <eko.mirhard@gmail.com>
2016-11-09 20:36:48 +0700

    Initial migration to the new board

---
+++ ios/backbone/BootLoaderService.h
@@ -0,0 +1,13 @@
+//
+//  BootLoaderService.h
+//  Backbone
+//
+//  Created by Eko Mirhard on 11/4/16.
+//  Copyright © 2016 Backbone Labs, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BootLoaderService : NSObject
+
+@end
---
+++ ios/backbone/BootLoaderService.m
@@ -0,0 +1,13 @@
+//
+//  BootLoaderService.m
+//  Backbone
+//
+//  Created by Eko Mirhard on 11/4/16.
+//  Copyright © 2016 Backbone Labs, Inc. All rights reserved.
+//
+
+#import "BootLoaderService.h"
+
+@implementation BootLoaderService
+
+@end
