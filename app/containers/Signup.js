2733cad8a54e0e3eb79642c89fbeb15b5a9782e7
Khoa Phan <kp@gobackbone.com>
2016-11-14 18:23:21 -0800

    Fix linting

--- app/containers/Signup.js
+++ app/containers/Signup.js
@@ -142,7 +142,12 @@ class Signup extends Component {
                         placeholder="Password"
                         keyboardType="default"
                         onChangeText={this.onPasswordChange}
-                        onSubmitEditing={((!email || !validEmail) || (!password || !validPassword)) ? null : this.signup}
+                        onSubmitEditing={
+                          ((!email || !validEmail) || (!password || !validPassword)) ?
+                          null
+                          :
+                            this.signup
+                        }
                         autoCorrect={false}
                         secureTextEntry
                         returnKeyType="go"
@@ -157,7 +162,11 @@ class Signup extends Component {
                         style={styles._CTAButton}
                         text="SIGN UP"
                         primary
-                        disabled={this.props.inProgress || ((!email || !validEmail) || (!password || !validPassword))}
+                        disabled={
+                          this.props.inProgress ||
+                          ((!email || !validEmail) ||
+                          (!password || !validPassword))
+                        }
                         onPress={this.signup}
                       />
                     </View>
