cfa907a2bdeb357f479a31710258157fa5cf1df9
Khoa Phan <kp@gobackbone.com>
2016-11-17 10:58:47 -0800

    Remove unused/nonexistent style props and imports

--- app/styles/device.js
+++ app/styles/device.js
@@ -1,7 +1,4 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
-import relativeDimensions from '../utils/relativeDimensions';
-
-const { heightDifference } = relativeDimensions;
 
 export default EStyleSheet.create({
   container: {
@@ -14,27 +11,5 @@ export default EStyleSheet.create({
   deviceID: {
     marginTop: '0.5rem',
     fontSize: '0.5rem',
-  },
-  backgroundImage: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-  },
-  deviceInfoContainer: {
-    flex: 0.55,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-  deviceInfo: {
-    alignItems: 'center',
-  },
-  deviceInfoBodyText: {
-    marginTop: 5 * heightDifference,
-    color: '$primaryFontColor',
-  },
-  buttonContainer: {
-    flex: 0.45,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
