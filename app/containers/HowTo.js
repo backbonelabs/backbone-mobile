61f1ed3c066d5f07b546ee0562c9f2966c7e7f94
Khoa Phan <kp@gobackbone.com>
2016-11-17 15:13:43 -0800

    General layout for `HowTo` scene

--- app/containers/HowTo.js
+++ app/containers/HowTo.js
@@ -1,47 +1,73 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Image,
+  ScrollView,
+  TouchableOpacity,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import gradientBackground20 from '../images/gradientBackground20.png';
-import styles from '../styles/support';
+import howToContent from '../images/howTo/';
+import styles from '../styles/howTo';
 import HeadingText from '../components/HeadingText';
 import BodyText from '../components/BodyText';
 
-class HowTo extends Component {
-  static propTypes = {
-    dispatch: PropTypes.func,
-    navigator: PropTypes.shape({
-      pop: PropTypes.func,
-    }),
-    support: PropTypes.shape({
-      inProgress: PropTypes.bool,
-      errorMessage: PropTypes.string,
-    }),
-    user: PropTypes.shape({
-      nickname: PropTypes.string,
-    }),
-  };
-
+export default class HowTo extends Component {
   constructor() {
     super();
+    this.state = {
+      pressedItem: null,
+    };
   }
 
   render() {
     return (
       <Image source={gradientBackground20} style={styles.background}>
-        <View>
-          <View style={{ flex: 0.6, height: 300, width: 300 }}/>
-          <View style={{ flex: 0.2, alignItems: 'center' }}>
-            <HeadingText size={2}>Putting on Backbone</HeadingText>
-          </View>
-          <View style={{ flex: 0.2, alignItems: 'center' }}>
-            <BodyText>Putting on Backbone</BodyText>
-          </View>
-        </View>
+        <ScrollView>
+          {
+            Object.keys(howToContent.GIF).map((value, key) => {
+              const contentKey = key + 1;
+
+              return (
+                <View key={key} style={styles.howToContainer}>
+                  <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={() => (
+                      this.setState({ pressedItem: this.state.pressedItem ? null : contentKey })
+                    )}
+                  >
+                    {
+                      this.state.pressedItem === contentKey ?
+                        <Image style={styles.gif} source={howToContent.GIF[contentKey]}>
+                          <Icon
+                            style={styles._pauseIconPadding}
+                            name={howToContent.PAUSE}
+                            size={styles._pauseIcon.width}
+                            color={styles._pauseIcon.color}
+                          />
+                        </Image>
+                        :
+                          <Image style={styles.png} source={howToContent.PNG[contentKey]}>
+                            <Icon
+                              name={howToContent.PLAY}
+                              size={styles._playIcon.height}
+                              color={styles._playIcon.color}
+                            />
+                          </Image>
+                    }
+                  </TouchableOpacity>
+                  <View style={styles.textContainer}>
+                    <HeadingText size={2}>{howToContent.TEXT[contentKey].title}</HeadingText>
+                  </View>
+                  <View style={styles.textContainer}>
+                    <BodyText>{howToContent.TEXT[contentKey].body}</BodyText>
+                  </View>
+                </View>
+              );
+            })
+          }
+        </ScrollView>
       </Image>
     );
   }
 }
-
-export default HowTo;
--- app/styles/howTo.js
+++ app/styles/howTo.js
@@ -1,5 +1,59 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
+import relativeDimensions from '../utils/relativeDimensions';
+
+const { widthDifference, heightDifference } = relativeDimensions;
+
+const absoluteCenter = {
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const relativeImageDimensions = {
+  width: 319 * widthDifference,
+  height: 239 * heightDifference,
+};
 
 export default EStyleSheet.create({
-
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  howToContainer: {
+    flex: 1,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '$activeBorderColor',
+    paddingVertical: 25 * heightDifference,
+  },
+  buttonContainer: {
+    flex: 0.8,
+    ...absoluteCenter,
+  },
+  gif: {
+    ...relativeImageDimensions,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  png: {
+    ...relativeImageDimensions,
+    ...absoluteCenter,
+  },
+  playIcon: {
+    color: '$primaryColor',
+    height: 100 * heightDifference,
+  },
+  pauseIcon: {
+    color: '$primaryColor',
+    width: 25 * widthDifference,
+  },
+  pauseIconPadding: {
+    paddingVertical: 5.5 * widthDifference,
+    paddingHorizontal: 7.5 * widthDifference,
+  },
+  textContainer: {
+    flex: 0.1,
+    paddingTop: 10 * heightDifference,
+    ...absoluteCenter,
+  },
 });
