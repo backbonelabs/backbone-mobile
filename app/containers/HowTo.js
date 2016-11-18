429c3bcd53f0356e971a6bb624d8bbd9ca1c512f
Khoa Phan <kp@gobackbone.com>
2016-11-18 11:07:49 -0800

    Changes:
    - Create a constant object for "how to" content
    - Re-arrange "how to" content
    - Switch out "pause" icon with "stop" icon

--- app/containers/HowTo.js
+++ app/containers/HowTo.js
@@ -6,11 +6,29 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import gradientBackground20 from '../images/gradientBackground20.png';
-import howToContent from '../images/howTo/';
 import styles from '../styles/howTo';
 import HeadingText from '../components/HeadingText';
 import BodyText from '../components/BodyText';
+import gradientBackground20 from '../images/gradientBackground20.png';
+import onePNG from '../images/howTo/PNG/one.png';
+import oneGIF from '../images/howTo/GIF/one.gif';
+import twoPNG from '../images/howTo/PNG/two.png';
+import twoGIF from '../images/howTo/GIF/two.gif';
+
+const howToContent = [
+  {
+    PNG: onePNG,
+    GIF: oneGIF,
+    title: 'Put On/Take Off',
+    body: 'How to put on/take off your Backbone',
+  },
+  {
+    PNG: twoPNG,
+    GIF: twoGIF,
+    title: 'Adjust',
+    body: 'How to adjust your Backbone',
+  },
+];
 
 export default class HowTo extends Component {
   constructor() {
@@ -25,49 +43,45 @@ export default class HowTo extends Component {
       <Image source={gradientBackground20} style={styles.background}>
         <ScrollView>
           {
-            Object.keys(howToContent.GIF).map((value, key) => {
-              const contentKey = key + 1;
-
-              return (
-                <View key={key} style={styles.howToContainer}>
-                  <TouchableOpacity
-                    style={styles.buttonContainer}
-                    onPress={() => (
-                      this.setState({ pressedItem: this.state.pressedItem === contentKey ?
-                        null
-                        :
-                          contentKey })
-                    )}
-                  >
-                    {
-                      this.state.pressedItem === contentKey ?
-                        <Image style={styles.gif} source={howToContent.GIF[contentKey]}>
+            howToContent.map((value, key) => (
+              <View key={key} style={styles.howToContainer}>
+                <View style={styles.textContainer}>
+                  <HeadingText size={2}>{value.title}</HeadingText>
+                </View>
+                <TouchableOpacity
+                  style={styles.buttonContainer}
+                  onPress={() => (
+                    this.setState({ pressedItem: this.state.pressedItem === key ?
+                      null
+                      :
+                        key })
+                  )}
+                >
+                  {
+                    this.state.pressedItem === key ?
+                      <Image style={styles.gif} source={value.GIF}>
+                        <Icon
+                          style={styles._pauseIconPadding}
+                          name={'stop'}
+                          size={styles._pauseIcon.height}
+                          color={styles._pauseIcon.color}
+                        />
+                      </Image>
+                      :
+                        <Image style={styles.png} source={value.PNG}>
                           <Icon
-                            style={styles._pauseIconPadding}
-                            name={howToContent.PAUSE}
-                            size={styles._pauseIcon.height}
-                            color={styles._pauseIcon.color}
+                            name={'play'}
+                            size={styles._playIcon.height}
+                            color={styles._playIcon.color}
                           />
                         </Image>
-                        :
-                          <Image style={styles.png} source={howToContent.PNG[contentKey]}>
-                            <Icon
-                              name={howToContent.PLAY}
-                              size={styles._playIcon.height}
-                              color={styles._playIcon.color}
-                            />
-                          </Image>
-                    }
-                  </TouchableOpacity>
-                  <View style={styles.textContainer}>
-                    <HeadingText size={2}>{howToContent.TEXT[contentKey].title}</HeadingText>
-                  </View>
-                  <View style={styles.textContainer}>
-                    <BodyText>{howToContent.TEXT[contentKey].body}</BodyText>
-                  </View>
+                  }
+                </TouchableOpacity>
+                <View style={styles.textContainer}>
+                  <BodyText>{value.body}</BodyText>
                 </View>
-              );
-            })
+              </View>
+            ))
           }
         </ScrollView>
       </Image>
