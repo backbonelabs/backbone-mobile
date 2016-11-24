8eb7d09db16de90a4dcd55723e4e5c1f60fb54e4
Khoa Phan <kp@gobackbone.com>
2016-11-23 22:17:01 -0800

    Refactor to work with new actions and store

--- app/containers/Device.js
+++ app/containers/Device.js
@@ -3,7 +3,6 @@ import {
   Image,
   View,
   Alert,
-  NativeModules,
 } from 'react-native';
 import { connect } from 'react-redux';
 import deviceActions from '../actions/device';
@@ -13,25 +12,7 @@ import Button from '../components/Button';
 import Spinner from '../components/Spinner';
 import BodyText from '../components/BodyText';
 import HeadingText from '../components/HeadingText';
-import constants from '../utils/constants';
-import SensitiveInfo from '../utils/SensitiveInfo';
 import routes from '../routes';
-
-const { DeviceManagementService } = NativeModules;
-
-// const DeviceInfoItem = props => (
-//   <View style={styles.deviceInfo}>
-//     <HeadingText size={3}>{props.headingText}</HeadingText>
-//     <BodyText style={styles._deviceInfoBodyText}>{props.bodyText}</BodyText>
-//     {props.children}
-//   </View>
-// );
-
-// DeviceInfoItem.propTypes = {
-//   children: PropTypes.node,
-//   headingText: PropTypes.string,
-//   bodyText: PropTypes.string,
-// };
 
 class Device extends Component {
   static propTypes = {
@@ -49,14 +30,6 @@ class Device extends Component {
 
   constructor() {
     super();
-    this.state = {
-      device: {
-        isPaired: false,
-        firmwareVersion: null,
-        updateAvailable: false,
-      },
-      inProgress: true,
-    };
 
     this.unpairDevice = this.unpairDevice.bind(this);
     this.addDevice = this.addDevice.bind(this);
@@ -64,31 +37,8 @@ class Device extends Component {
   }
 
   componentWillMount() {
-    if (this.props.isConnected) {
-      // Get latest device information if it's currently connected
-      this.props.dispatch(deviceActions.getInfo());
-    } else {
-      // If device isn't currently connected, fetch locally stored data, if any
-      SensitiveInfo.getItem(constants.storageKeys.DEVICE)
-        .then(device => {
-          const stateData = { inProgress: false };
-          if (device) {
-            stateData.device = device;
-          }
-          this.setState(stateData);
-        });
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { inProgress, device } = nextProps;
-    if (this.props.inProgress && !inProgress) {
-      // Save latest device information in state and local store
-      this.setState({
-        device,
-        inProgress,
-      });
-    }
+    // Get latest device information if it's currently connected
+    this.props.dispatch(deviceActions.getInfo(this.props.isConnected));
   }
 
   unpairDevice() {
@@ -98,43 +48,9 @@ class Device extends Component {
       'This will remove your Backbone',
       [
         { text: 'Cancel' },
-        { text: 'Unpair',
-          onPress: () => {
-            // Reset state to defaults
-            this.setState({ inProgress: true }, () => (
-              DeviceManagementService.forgetDevice(error => {
-                if (error) {
-                  Alert.alert(
-                    'Error',
-                    'There was a problem unpairing your Backbone',
-                    [{
-                      text: 'Try Again',
-                      onPress: () => this.setState({ inProgress: false }),
-                    }],
-                  );
-                } else {
-                  Alert.alert(
-                    'Success',
-                    'You have unpaired your Backbone',
-                    [{
-                      text: 'OK',
-                      onPress: () => (
-                        // Set device state back to defaults
-                        this.setState({
-                          device: {
-                            isPaired: false,
-                            firmwareVersion: null,
-                            updateAvailable: false,
-                          },
-                          inProgress: false,
-                        })
-                      ),
-                    }]
-                  );
-                }
-              })
-            ));
-          },
+        {
+          text: 'Unpair',
+          onPress: () => this.props.dispatch(deviceActions.forget()),
         },
       ]
     );
@@ -151,24 +67,18 @@ class Device extends Component {
           'Error',
           'Device not found, please connect to your Backbone before updating.',
         [
-          {
-            text: 'Cancel',
-          },
+          { text: 'Cancel' },
           {
             text: 'Connect',
             onPress: () => this.props.navigator.push(routes.deviceAdd),
           },
         ]
       );
-    } else {
-      this.setState({ inProgress: true }, () => {
-        // TODO: Call native method here to update user's firmware
-      });
     }
   }
 
   render() {
-    const { device, inProgress } = this.state;
+    const { device, inProgress } = this.props;
 
     return inProgress ?
       <Spinner />
@@ -177,22 +87,24 @@ class Device extends Component {
           <View style={styles.deviceInfoContainer}>
             <Image source={sensorSmall} style={styles.sensorImage} />
             <HeadingText size={3}>
-              Firmware Version: { device.firmwareVersion || 'n/a' }
+              Firmware Version: { (device && device.firmwareVersion) ? device.firmwareVersion
+                : 'n/a'
+              }
             </HeadingText>
-            { device.updateAvailable &&
+            { device && device.updateAvailable &&
               <BodyText style={styles._deviceInfoBodyText}>
                 (Update Available)
               </BodyText>
             }
           </View>
           <View style={styles.buttonContainer}>
-            { device.isPaired ?
+            { device && device.firmwareVersion ?
               <Button primary text="UNPAIR" onPress={this.unpairDevice} />
               :
                 <Button primary text="ADD NEW" onPress={this.addDevice} />
             }
             { /* Only show this button if there's a firmware update available */
-              device.updateAvailable && (
+              device && device.updateAvailable && (
                 <Button
                   style={styles._updateButton}
                   text="UPDATE"
