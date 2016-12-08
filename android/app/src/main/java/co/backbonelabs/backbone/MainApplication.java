a31940ab5e046b12349d8f2be3eab35f44f6ecf4
Khoa Phan <kp@gobackbone.com>
2016-12-08 10:34:12 -0800

    Add react-native-material-kit and link to project

--- android/app/src/main/java/co/backbonelabs/backbone/MainApplication.java
+++ android/app/src/main/java/co/backbonelabs/backbone/MainApplication.java
@@ -3,6 +3,7 @@ package co.backbonelabs.backbone;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.horcrux.svg.RNSvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new ReactMaterialKitPackage(),
             new RNSvgPackage(),
         new RNSensitiveInfoPackage(),
         new BackbonePackage()
