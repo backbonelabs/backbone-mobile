8181074823f707e337ff54d4e062e96dafc14ecf
Kevin Huang <huang.kev@gmail.com>
2016-12-10 01:34:58 -0800

    Add util for a responsive font size unaffected by system scaling

--- app/utils/relativeDimensions.js
+++ app/utils/relativeDimensions.js
@@ -1,4 +1,7 @@
-import { Dimensions } from 'react-native';
+import {
+  Dimensions,
+  PixelRatio,
+} from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 const widthDifference = width / 375;
@@ -6,14 +9,28 @@ const heightDifference = height / 667;
 
 /**
  * Applies widthDifference to input measurement value
- * @param  {Number} input  Measurement (pixels)
- * @return {Number}        Input with widthDifference applied
+ * @param  {Number} input Measurement (density-independent pixels)
+ * @return {Number}       Input with widthDifference applied
  */
 
 const applyWidthDifference = input => input * widthDifference;
+
+/**
+ * Returns a font size scaled based on the widthDifference, and the font size
+ * is not affected by the phone's font scaling factor.
+ * On iOS, this is essentially a no-op since font scale will always be the same
+ * as the device pixel density. On Android, this will adjust the font size based
+ * on the system's font scaling factor so the font size will look the same
+ * regardless of device screen size and scaling factors.
+ * @param  {Number} size The original or base font size in density-independent pixels
+ * @return {Number}      The adjusted font size
+ */
+const fixedResponsiveFontSize = size =>
+  applyWidthDifference(size) * (PixelRatio.get() / PixelRatio.getFontScale());
 
 export default {
   widthDifference,
   heightDifference,
   applyWidthDifference,
+  fixedResponsiveFontSize,
 };
