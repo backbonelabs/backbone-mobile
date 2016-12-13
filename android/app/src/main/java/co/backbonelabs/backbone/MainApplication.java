266b4b5e6cc94a2b779f1def5603c9b1ec2456de
Khoa Phan <kp@gobackbone.com>
2016-12-13 13:44:32 -0800

    Link react-native-fs with project

--- android/app/src/main/java/co/backbonelabs/backbone/MainApplication.java
+++ android/app/src/main/java/co/backbonelabs/backbone/MainApplication.java
@@ -3,6 +3,7 @@ package co.backbonelabs.backbone;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.horcrux.svg.RNSvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RNFSPackage(),
             new ReactMaterialKitPackage(),
             new RNSvgPackage(),
         new RNSensitiveInfoPackage(),
