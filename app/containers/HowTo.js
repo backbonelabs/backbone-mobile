0f1951a75a20f4f644e98dcfae3185d24d19f5e8
Khoa Phan <kp@gobackbone.com>
2016-12-23 16:19:30 -0800

    Use Mixpanel util

--- app/containers/HowTo.js
+++ app/containers/HowTo.js
@@ -5,6 +5,7 @@ import {
   ScrollView,
   TouchableOpacity,
 } from 'react-native';
+import autobind from 'autobind-decorator';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from '../styles/howTo';
 import HeadingText from '../components/HeadingText';
@@ -14,19 +15,20 @@ import onePNG from '../images/howTo/PNG/one.png';
 import oneGIF from '../images/howTo/GIF/one.gif';
 import twoPNG from '../images/howTo/PNG/two.png';
 import twoGIF from '../images/howTo/GIF/two.gif';
+import Mixpanel from '../utils/Mixpanel';
 
 const howToContent = [
   {
     PNG: onePNG,
     GIF: oneGIF,
     title: 'Put On/Take Off',
-    body: 'How to put on/take off your Backbone',
+    description: 'How to put on/take off your Backbone',
   },
   {
     PNG: twoPNG,
     GIF: twoGIF,
     title: 'Adjust',
-    body: 'How to adjust your Backbone',
+    description: 'How to adjust your Backbone',
   },
 ];
 
@@ -38,50 +40,68 @@ export default class HowTo extends Component {
     };
   }
 
+  /**
+   * Tracks the content that a user views
+   */
+  @autobind
+  trackContentView(videoProperties) {
+    const { title, description } = videoProperties;
+    Mixpanel.trackWithProperties('contentView', {
+      contentType: 'video',
+      videoProperties: { title, description },
+    });
+  }
+
   render() {
     return (
       <Image source={gradientBackground20} style={styles.background}>
         <ScrollView>
           {
-            howToContent.map((value, key) => (
-              <View key={key} style={styles.howToContainer}>
-                <View style={styles.textContainer}>
-                  <HeadingText size={2}>{value.title}</HeadingText>
-                </View>
-                <TouchableOpacity
-                  style={styles.buttonContainer}
-                  onPress={() => (
-                    this.setState({ pressedItem: this.state.pressedItem === key ?
-                      null
-                      :
-                        key })
-                  )}
-                >
-                  {
-                    this.state.pressedItem === key ?
-                      <Image style={styles.gif} source={value.GIF}>
-                        <Icon
-                          style={styles._stopIconPadding}
-                          name="stop"
-                          size={styles._stopIcon.height}
-                          color={styles._stopIcon.color}
-                        />
-                      </Image>
-                      :
-                        <Image style={styles.png} source={value.PNG}>
+            howToContent.map((value, key) => {
+              const currentlyPlaying = this.state.pressedItem === key;
+
+              return (
+                <View key={key} style={styles.howToContainer}>
+                  <View style={styles.textContainer}>
+                    <HeadingText size={2}>{value.title}</HeadingText>
+                  </View>
+                  <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={() => (
+                      this.setState({
+                        pressedItem: currentlyPlaying ? null : key,
+                      }, () => (
+                        // Track only if video isn't currently playing
+                        !currentlyPlaying && this.trackContentView(value)
+                      ))
+                    )}
+                  >
+                    {
+                      currentlyPlaying ?
+                        <Image style={styles.gif} source={value.GIF}>
                           <Icon
-                            name="play"
-                            size={styles._playIcon.height}
-                            color={styles._playIcon.color}
+                            style={styles._stopIconPadding}
+                            name="stop"
+                            size={styles._stopIcon.height}
+                            color={styles._stopIcon.color}
                           />
                         </Image>
-                  }
-                </TouchableOpacity>
-                <View style={styles.textContainer}>
-                  <BodyText>{value.body}</BodyText>
+                        :
+                          <Image style={styles.png} source={value.PNG}>
+                            <Icon
+                              name="play"
+                              size={styles._playIcon.height}
+                              color={styles._playIcon.color}
+                            />
+                          </Image>
+                    }
+                  </TouchableOpacity>
+                  <View style={styles.textContainer}>
+                    <BodyText>{value.description}</BodyText>
+                  </View>
                 </View>
-              </View>
-            ))
+              );
+            })
           }
         </ScrollView>
       </Image>
