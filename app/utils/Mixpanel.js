3b4ef3bde495ba1ab3561feb0f524991981d5980
Khoa Phan <kp@gobackbone.com>
2016-12-30 11:58:53 -0800

    Create setUserProperty method

--- app/utils/Mixpanel.js
+++ app/utils/Mixpanel.js
@@ -1,4 +1,6 @@
 import { NativeModules } from 'react-native';
+import { map } from 'lodash';
+import constants from './constants';
 
 const { Mixpanel, BluetoothService } = NativeModules;
 
@@ -17,6 +19,61 @@ export default {
    */
   set(userDetails) {
     Mixpanel.set(userDetails);
+  },
+
+  /**
+   * Formats the user profile properties before setting on Mixpanel
+   * @param  {Object}  userDetails  User profile properties
+   */
+  setUserProperties(user) {
+    const userClone = Object.assign({ ...user }, { ...user.settings });
+    const { gender, heightUnitPreference, weightUnitPreference } = user;
+    const { height, weight } = constants;
+    const formattedGender = gender === constants.gender.male ? 'male' : 'female';
+
+    // Delete unwanted profile properties
+    [
+      '_id',
+      'confirmationToken',
+      'confirmationTokenExpiry',
+      'firstName',
+      'lastName',
+      'nickname',
+      'settings',
+    ].forEach(value => {
+      delete userClone[value];
+    });
+
+
+    map(userClone, (value, key) => {
+      if (value || value === false) {
+        if (key === 'email') {
+          // Set Mixpanel special property $email and delete unused email property
+          userClone.$email = user[key];
+          delete userClone[key];
+        } else if (value && key === 'gender') {
+          // Change gender from integer to word equivalent ("male" / "female")
+          userClone.gender = formattedGender;
+        } else if (key === 'height') {
+          // Change heightUnitPreference from integer to word equivalent ("IN" / "CM")
+          userClone.heightUnitPreference = heightUnitPreference === height.units.IN ? 'IN' : 'CM';
+        } else if (key === 'weight') {
+          // Change heightUnitPreference from integer to word equivalent ("LB" / "KG")
+          userClone.weightUnitPreference = weightUnitPreference === weight.units.LB ? 'LB' : 'KG';
+        }
+      } else {
+        if (key === 'weight' || key === 'height') {
+          // These unit preference properties will never be undefined
+          // Remove them if their measurement values are undefined
+          delete userClone[`${key}UnitPreference`];
+        }
+
+        // Remove properties with undefined values
+        delete userClone[key];
+      }
+    });
+
+    this.set(userClone);
   },
 
   /**
