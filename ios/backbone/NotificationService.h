57f684f8ba5a110e967c3ae0d3cfc9f3260fd73c
Eko Mirhard <eko.mirhard@gmail.com>
2017-01-04 23:46:53 +0700

    Added NotificationService for iOS

---
+++ ios/backbone/NotificationService.h
@@ -0,0 +1,14 @@
+//
+//  NotificationService.h
+//  Backbone
+//
+//  Created by Eko Mirhard on 1/4/17.
+//  Copyright © 2017 Backbone Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RCTBridgeModule.h"
+
+@interface NotificationService : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/backbone/NotificationService.m
@@ -0,0 +1,34 @@
+//
+//  NotificationService.m
+//  Backbone
+//
+//  Created by Eko Mirhard on 1/4/17.
+//  Copyright © 2017 Backbone Labs, Inc. All rights reserved.
+//
+
+#import "NotificationService.h"
+
+@implementation NotificationService
+
+- (id)init {
+  return self;
+}
+
+RCT_EXPORT_MODULE();
+
+/**
+ Send a local push notification
+ @param message The message to be displayed on the push notification
+ */
+RCT_EXPORT_METHOD(sendLocalNotification:(NSString*)message) {
+  UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+  
+  if (localNotification) {
+    localNotification.alertBody = NSLocalizedString(message, nil);
+    localNotification.soundName = UILocalNotificationDefaultSoundName;
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+  }
+}
+
+@end
