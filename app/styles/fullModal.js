75b96ca49f6ed3868f0e0e51b2381b0fd09ce659
Khoa Phan <kp@gobackbone.com>
2017-01-18 15:08:45 -0800

    Remove width and height properties for container prop, and remove

--- app/styles/fullModal.js
+++ app/styles/fullModal.js
@@ -1,4 +1,3 @@
-import { Dimensions } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import relativeDimensions from '../utils/relativeDimensions';
 
@@ -6,11 +5,8 @@ const { applyWidthDifference, heightDifference, fixedResponsiveFontSize } = rela
 
 export default EStyleSheet.create({
   $iconSize: fixedResponsiveFontSize(40),
-  $windowHeight: Dimensions.get('window').height,
   container: {
     flex: 1,
-    width: '100%',
-    height: '$windowHeight - $statusBarHeight',
     paddingHorizontal: applyWidthDifference(10),
     paddingVertical: 10 * heightDifference,
   },
