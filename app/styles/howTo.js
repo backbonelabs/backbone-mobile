5e11913b43f0f61906dfc028cf51548c88e8284c
Khoa Phan <kp@gobackbone.com>
2017-01-22 22:04:18 -0800

    Remove unused style props

--- app/styles/howTo.js
+++ app/styles/howTo.js
@@ -1,54 +1,22 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 import relativeDimensions from '../utils/relativeDimensions';
 
-const { applyWidthDifference, heightDifference, fixedResponsiveFontSize } = relativeDimensions;
+const { applyWidthDifference } = relativeDimensions;
 
 const absoluteCenter = {
   alignItems: 'center',
   justifyContent: 'center',
 };
 
-const relativeImageDimensions = {
-  width: applyWidthDifference(319),
-  height: applyWidthDifference(239),
-};
-
 export default EStyleSheet.create({
   howToContainer: {
-    flex: 1,
-    alignItems: 'center',
     borderBottomWidth: 1,
     borderColor: '$activeBorderColor',
-    paddingVertical: 5 * heightDifference,
-  },
-  buttonContainer: {
-    flex: 0.8,
-    ...absoluteCenter,
-  },
-  gif: {
-    ...relativeImageDimensions,
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
-  },
-  png: {
-    ...relativeImageDimensions,
-    ...absoluteCenter,
-  },
-  playIcon: {
-    color: '$primaryColor',
-    height: fixedResponsiveFontSize(100),
-  },
-  stopIcon: {
-    color: '$primaryColor',
-    height: fixedResponsiveFontSize(25),
-  },
-  stopIconPadding: {
-    paddingVertical: 5.5 * heightDifference,
-    paddingHorizontal: applyWidthDifference(7.5),
+    paddingVertical: applyWidthDifference(15),
   },
   textContainer: {
     flex: 0.1,
-    paddingVertical: 10 * heightDifference,
+    paddingVertical: applyWidthDifference(10),
     ...absoluteCenter,
   },
 });
