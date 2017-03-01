ccf5a48d426c560cee654c65fd4afd9a6ac62a57
Miguel Correa <miguelc1221@gmail.com>
2017-02-28 23:14:13 -0500

    add util functions

---
+++ app/components/utils/convertToHMS.js
@@ -0,0 +1,7 @@
+export default (secs) => {
+  const secNum = parseInt(secs, 10);
+  const hours = Math.floor(secNum / 3600) % 24;
+  const minutes = Math.floor(secNum / 60) % 60;
+  const seconds = secNum % 60;
+  return `${hours}h ${minutes}m ${seconds}s`;
+};
