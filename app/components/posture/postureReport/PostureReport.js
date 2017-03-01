5c8fd3b7f0f99b24cbcd1cc8b6fd65ba0cf5f407
Miguel Correa <miguelc1221@gmail.com>
2017-02-28 23:15:07 -0500

    add postureReport component

---
+++ app/components/posture/postureReport/PostureReport.js
@@ -0,0 +1,198 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  View,
+  InteractionManager,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import autobind from 'autobind-decorator';
+import { VictoryPie } from 'victory-native';
+import { connect } from 'react-redux';
+import styles from '../../../styles/posture/postureReport';
+import HeadingText from '../../../components/HeadingText';
+import color from '../../../styles/theme';
+import userActions from '../../../actions/user';
+import BodyText from '../../BodyText';
+import Spinner from '../../Spinner';
+import convertToHMS from '../../utils/convertToHMS';
+import getDaysAgo from '../../utils/getDaysAgo';
+
+class PostureReport extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func,
+    sessions: PropTypes.array,
+    loading: PropTypes.bool,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessions: [],
+      sessionIndex: 0,
+      from_date: getDaysAgo(7),
+      to_date: getDaysAgo(0),
+      days: 7,
+    };
+  }
+
+  componentDidMount() {
+    InteractionManager.runAfterInteractions(() => {
+      this.props.dispatch(userActions.fetchUserSessions({
+        from_date: this.state.from_date,
+        to_date: this.state.to_date,
+      }));
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.sessions !== nextProps.sessions) {
+      this.setState({ sessions: nextProps.sessions[this.state.sessionIndex] });
+    }
+  }
+
+  @autobind
+  onBackPress() {
+    const sessionLength = this.props.sessions.length;
+
+    // if there is no session, don't do anything
+    if (!this.state.sessions) {
+      return null;
+    }
+
+    // if the index is the last one in the sessions array
+    if ((this.state.sessionIndex + 1) === sessionLength) {
+      // fetch the sessions within 7 days before
+      this.props.dispatch(userActions.fetchUserSessions({
+        from_date: getDaysAgo(this.state.days + 7),
+        to_date: getDaysAgo(this.state.days),
+      }));
+      this.setState({
+        sessionIndex: this.state.sessionIndex + 1,
+        days: this.state.days + 7,
+      });
+    } else {
+      this.setState({
+        sessionIndex: this.state.sessionIndex + 1,
+        sessions: this.props.sessions[this.state.sessionIndex + 1],
+      });
+    }
+  }
+
+  @autobind
+  onForwardPress() {
+    this.setState({
+      sessionIndex: this.state.sessionIndex - 1,
+      sessions: this.props.sessions[this.state.sessionIndex - 1],
+    });
+  }
+
+  render() {
+    const { sessions, sessionIndex } = this.state;
+    // only show the right arrow if there is a more recent session
+    const rightArrow = (sessionIndex === 0) ? <View style={styles.icons} /> :
+      <TouchableOpacity
+        onPress={this.onForwardPress}
+      >
+        <View>
+          <Icon
+            name="angle-right"
+            size={40} color="#900"
+            style={styles.icons}
+          />
+        </View>
+      </TouchableOpacity>;
+
+    const data = {
+      date: 'No sessions',
+      chartData: [{ label: null }],
+    };
+
+    if (sessions) {
+      data.date = new Date(sessions.time).toDateString();
+      if (sessions.properties) {
+        const { totalDuration, slouchTime } = sessions.properties;
+        data.chartData = [
+          {
+            label: 'Good',
+            duration: totalDuration - slouchTime,
+          },
+          {
+            label: 'Poor',
+            duration: slouchTime,
+          },
+        ];
+        data.total = convertToHMS(totalDuration);
+        data.good = convertToHMS(totalDuration - slouchTime);
+        data.bad = convertToHMS(slouchTime);
+      }
+    }
+
+    // return a spinner if component is fetching data
+    if (this.props.loading) {
+      return <Spinner />;
+    }
+
+    return (
+      <View style={styles.container}>
+        <HeadingText size={1} style={styles._heading}>Posture Report</HeadingText>
+        <View style={styles.dateContainer}>
+          <TouchableOpacity
+            onPress={this.onBackPress}
+          >
+            <View>
+              <Icon
+                name="angle-left"
+                size={40}
+                color="#900"
+                style={styles.icons}
+              />
+            </View>
+          </TouchableOpacity>
+          <HeadingText size={2}>{data.date}</HeadingText>
+          {rightArrow}
+        </View>
+        <View style={styles.chart}>
+          <VictoryPie
+            style={{
+              labels: { fontSize: 14, fontWeight: 'bold' },
+            }}
+            data={data.chartData}
+            innerRadius={80}
+            x="label"
+            y="duration"
+            width={300}
+            padding={{ top: 80, bottom: 0, left: 50, right: 50 }}
+            animate={{
+              duration: 700,
+            }}
+            colorScale={[
+              'white',
+              color.primaryColor,
+            ]}
+          />
+          <View style={styles._total}>
+            <HeadingText size={1}>{data.total ? 'Total' : ''}</HeadingText>
+            <HeadingText size={1}>{data.total}</HeadingText>
+          </View>
+        </View>
+        <View style={styles._statsContainer}>
+          <View style={styles.innerContainer}>
+            <BodyText style={styles._statsGoodHeader}>Good</BodyText>
+            <BodyText style={styles._statsText}>{data.good}</BodyText>
+          </View>
+          <View style={styles.innerContainer}>
+            <BodyText style={styles._statsBadHeader}>Bad</BodyText>
+            <BodyText style={styles._statsText}>{data.bad}</BodyText>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  sessions: state.user.sessions,
+  loading: state.user.isUpdating,
+});
+
+export default connect(mapStateToProps)(PostureReport);
---
+++ app/styles/posture/postureReport.js
@@ -0,0 +1,70 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+import relativeDimensions from '../../utils/relativeDimensions';
+
+const { applyWidthDifference, heightDifference } = relativeDimensions;
+
+const statsText = {
+  textAlign: 'center',
+  paddingTop: 5,
+  paddingBottom: 5,
+};
+
+export default EStyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dateContainer: {
+    flexDirection: 'row',
+    width: '95%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flex: 2,
+    marginBottom: 5 * heightDifference,
+    backgroundColor: '$bannerColor',
+  },
+  icons: {
+    paddingRight: applyWidthDifference(25),
+    paddingLeft: applyWidthDifference(25),
+  },
+  chart: {
+    width: '95%',
+    backgroundColor: '$bannerColor',
+    alignItems: 'center',
+    flex: 15,
+  },
+  heading: {
+    textAlign: 'center',
+    paddingBottom: 25 * heightDifference,
+    marginTop: 30 * heightDifference,
+  },
+  total: {
+    position: 'relative',
+    alignItems: 'center',
+    bottom: 270,
+  },
+  statsContainer: {
+    flexDirection: 'row',
+    width: '95%',
+    marginTop: 5 * heightDifference,
+    justifyContent: 'space-between',
+    flex: 5,
+  },
+  innerContainer: {
+    width: applyWidthDifference(150),
+    height: 70 * heightDifference,
+    backgroundColor: '$bannerColor',
+  },
+  statsText: { ...statsText },
+  statsGoodHeader: {
+    ...statsText,
+    color: 'white',
+    backgroundColor: '#BDBDBD',
+  },
+  statsBadHeader: {
+    ...statsText,
+    color: '$primaryColor',
+    backgroundColor: '#BDBDBD',
+  },
+});
