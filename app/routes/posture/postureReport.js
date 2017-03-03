734faca3d8e2b61916b173d639b73f7830d6f55d
Miguel Correa <miguelc1221@gmail.com>
2017-03-03 18:04:51 -0500

    add title and backbutton

--- app/routes/posture/postureReport.js
+++ app/routes/posture/postureReport.js
@@ -3,4 +3,6 @@ import PostureReport from '../../components/posture/postureReport/PostureReport'
 export default {
   name: 'postureReport',
   component: PostureReport,
+  title: 'Session',
+  showBackButton: true,
 };
