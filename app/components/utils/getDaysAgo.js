26d5fd3727800db552ba897d11df309529b268e8
Miguel Correa <miguelc1221@gmail.com>
2017-03-04 19:49:16 -0500

    removed unneeded lines after merging api pr

--- app/components/utils/getDaysAgo.js
+++ app/components/utils/getDaysAgo.js
@@ -2,12 +2,5 @@ export default (days) => {
   const today = new Date();
   today.setDate(today.getDate() - days);
 
-  let month = (today.getMonth() + 1);
-  let day = today.getDate();
-  const year = today.getFullYear();
-
-  if (month.length < 2) month = `0${month}`;
-  if (day.length < 2) day = `0${day}`;
-
-  return `${year}-${month}-${day}`;
+  return today.toISOString();
 };
