1f521d5a82ed2e6f34f1591a19a160b724fafb81
Miguel Correa <miguelc1221@gmail.com>
2017-03-07 19:04:22 -0500

    add posture components

---
+++ app/components/posture/postureReport/PostureChart.js
@@ -0,0 +1,82 @@
+import React, { PropTypes } from 'react';
+import { View } from 'react-native';
+import { VictoryPie } from 'victory-native';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import styles from '../../../styles/posture/postureChart';
+import HeadingText from '../../../components/HeadingText';
+import color from '../../../styles/theme';
+import BodyText from '../../BodyText';
+import convertToHMS from '../../utils/convertToHMS';
+
+const PostureChart = (props) => {
+  const { sessionDate: { totalDuration, slouchTime, timestamp } } = props;
+  const data = {};
+  data.date = moment(timestamp).format('MMM Do YY');
+  data.chartData = [
+    {
+      label: 'Good',
+      duration: totalDuration - slouchTime,
+    },
+    {
+      label: 'Poor',
+      duration: slouchTime,
+    },
+  ];
+  data.total = convertToHMS(totalDuration);
+  data.good = convertToHMS(totalDuration - slouchTime);
+  data.bad = convertToHMS(slouchTime);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.dateContainer}>
+        <HeadingText size={2}>{data.date}</HeadingText>
+      </View>
+      <View style={styles.chart}>
+        <VictoryPie
+          style={{
+            labels: { fontSize: 14, fontWeight: 'bold' },
+          }}
+          innerRadius={80}
+          data={data.chartData}
+          x="label"
+          y="duration"
+          width={350}
+          padding={{ top: 80, bottom: 0, left: 70, right: 70 }}
+          animate={{
+            duration: 700,
+          }}
+          colorScale={[
+            'white',
+            color.primaryColor,
+          ]}
+        />
+        <View style={styles._total}>
+          <HeadingText size={1}>Total</HeadingText>
+          <HeadingText size={1}>{data.total}</HeadingText>
+        </View>
+      </View>
+      <View style={styles._statsContainer}>
+        <View style={styles.innerContainer}>
+          <BodyText style={styles._statsGoodHeader}>Good</BodyText>
+          <BodyText style={styles._statsText}>{data.good}</BodyText>
+        </View>
+        <View style={styles.innerContainer}>
+          <BodyText style={styles._statsBadHeader}>Bad</BodyText>
+          <BodyText style={styles._statsText}>{data.bad}</BodyText>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+PostureChart.propTypes = {
+  dispatch: PropTypes.func,
+  sessionDate: PropTypes.object,
+};
+
+const mapStateToProps = (state) => ({
+  sessionDate: state.user.sessionDate,
+});
+
+export default connect(mapStateToProps)(PostureChart);
--- app/components/posture/postureReport/PostureReport.js
+++ app/components/posture/postureReport/PostureReport.js
@@ -2,36 +2,55 @@ import React, { Component, PropTypes } from 'react';
 import {
   View,
   InteractionManager,
-  TouchableOpacity,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Calendar from 'react-native-calendar';
 import autobind from 'autobind-decorator';
-import { VictoryPie } from 'victory-native';
+import moment from 'moment';
 import { connect } from 'react-redux';
 import styles from '../../../styles/posture/postureReport';
 import HeadingText from '../../../components/HeadingText';
 import color from '../../../styles/theme';
 import userActions from '../../../actions/user';
-import BodyText from '../../BodyText';
+import postureActions from '../../../actions/posture';
 import Spinner from '../../Spinner';
-import convertToHMS from '../../utils/convertToHMS';
-import getDaysAgo from '../../utils/getDaysAgo';
+import routes from '../../../routes';
+
+const calendarStyles = {
+  calendarContainer: {
+    backgroundColor: 'transparent',
+  },
+  currentDayCircle: {
+    backgroundColor: 'black',
+  },
+  currentDayText: {
+    color: color.primaryColor,
+  },
+  hasEventCircle: {
+    backgroundColor: color.primaryColor,
+  },
+  hasEventText: {
+    color: 'white',
+  },
+};
 
 class PostureReport extends Component {
   static propTypes = {
     dispatch: PropTypes.func,
     sessions: PropTypes.array,
     loading: PropTypes.bool,
+    navigator: PropTypes.shape({
+      push: PropTypes.func,
+    }),
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      sessions: [],
-      sessionIndex: 0,
-      from_date: getDaysAgo(7),
-      to_date: getDaysAgo(0),
-      days: 7,
+      sessions: {},
+      startDate: moment().format('YYYY-MM-DD'),
+      from_date: moment().subtract(3, 'months').startOf('month').format('YYYY-MM-DD'),
+      to_date: moment().endOf('month').format('YYYY-MM-DD'),
+      loading: true,
     };
   }
 
@@ -41,179 +60,119 @@ class PostureReport extends Component {
         from_date: this.state.from_date,
         to_date: this.state.to_date,
       }));
+      this.setState({ loading: false });
     });
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.sessions !== nextProps.sessions) {
-      this.setState({ sessions: nextProps.sessions[this.state.sessionIndex] });
+      const groupByDay = nextProps.sessions
+      .sort((a, b) => b.timestamp - a.timestamp) // sort from latest to oldest
+      .reduce((acc, val) => {
+        const date = moment(val.timestamp).format('YYYY-MM-DD');
+        /* eslint-disable */
+        if (acc[date]) {
+          acc[date].sessionTime += val.sessionTime;
+          acc[date].slouchTime += val.slouchTime;
+          acc[date].totalDuration += val.totalDuration;
+
+          return acc;
+        }
+
+        acc[date] = val;
+        /* eslint-enable */
+        return acc;
+      }, {});
+      this.setState({ sessions: groupByDay });
     }
   }
 
   @autobind
-  onBackPress() {
-    const sessionLength = this.props.sessions.length;
+  onDateSelect(date) {
+    const formatDate = moment(date).format('YYYY-MM-DD');
+    const selectedSession = this.state.sessions[formatDate];
 
-    // if there is no session, don't do anything
-    if (!this.state.sessions) {
-      return null;
+    if (selectedSession) {
+      this.props.dispatch(postureActions.setSessionDate(selectedSession));
+      return this.props.navigator.push(routes.postureChart);
     }
 
-    // if the index is the last one in the sessions array
-    if ((this.state.sessionIndex + 1) === sessionLength) {
-      // fetch the sessions within 7 days before
+    return null;
+  }
+
+  @autobind
+  onTouchPrev(date) {
+    const selectedMonth = date.startOf('month').format('YYYY-MM-DD');
+    const fromDateMonth = moment(this.state.startDate)
+      .startOf('month').subtract(3, 'months').format('YYYY-MM-DD');
+    const toDateMonth = moment(this.state.startDate)
+      .startOf('month').subtract(1, 'months').format('YYYY-MM-DD');
+
+    if (selectedMonth === fromDateMonth) {
       this.props.dispatch(userActions.fetchUserSessions({
-        from_date: getDaysAgo(this.state.days + 7),
-        to_date: getDaysAgo(this.state.days),
+        from_date: fromDateMonth,
+        to_date: toDateMonth,
       }));
-      this.setState({
-        sessionIndex: this.state.sessionIndex + 1,
-        days: this.state.days + 7,
-      });
-    } else {
-      this.setState({
-        sessionIndex: this.state.sessionIndex + 1,
-        sessions: this.props.sessions[this.state.sessionIndex + 1],
+      return this.setState({
+        startDate: fromDateMonth,
+        from_date: fromDateMonth,
+        to_date: toDateMonth,
       });
     }
+
+    return null;
   }
 
   @autobind
-  onForwardPress() {
-    this.setState({
-      sessionIndex: this.state.sessionIndex - 1,
-      sessions: this.props.sessions[this.state.sessionIndex - 1],
-    });
+  onTouchNext(date) {
+    const selectedMonth = date.startOf('month').format('YYYY-MM-DD');
+    const fromDateMonth = moment(this.state.startDate)
+      .startOf('month').add(3, 'months').format('YYYY-MM-DD');
+    const toDateMonth = moment(this.state.startDate)
+      .startOf('month').add(6, 'months').format('YYYY-MM-DD');
+
+    if (selectedMonth === fromDateMonth) {
+      this.props.dispatch(userActions.fetchUserSessions({
+        from_date: fromDateMonth,
+        to_date: toDateMonth,
+      }));
+      return this.setState({
+        startDate: fromDateMonth,
+        from_date: fromDateMonth,
+        to_date: toDateMonth,
+      });
+    }
+
+    return null;
   }
 
   render() {
-    const { sessions, sessionIndex } = this.state;
-    // only show the right arrow if there is a more recent session
-    const rightArrow = (sessionIndex === 0) ? <View style={styles.icons} /> :
-      <TouchableOpacity
-        onPress={this.onForwardPress}
-      >
-        <View>
-          <Icon
-            name="angle-right"
-            size={40} color="#900"
-            style={styles.icons}
-          />
-        </View>
-      </TouchableOpacity>;
-
-    const data = {
-      date: 'No sessions',
-      chartData: [{ label: null }],
-    };
-
-    if (sessions) {
-      const { totalDuration, slouchTime, timestamp } = sessions;
-      data.date = new Date(timestamp).toDateString();
-      data.chartData = [
-        {
-          label: 'Good',
-          duration: totalDuration - slouchTime,
-        },
-        {
-          label: 'Poor',
-          duration: slouchTime,
-        },
-      ];
-      data.total = convertToHMS(totalDuration);
-      data.good = convertToHMS(totalDuration - slouchTime);
-      data.bad = convertToHMS(slouchTime);
-    }
-
+    const events = (Object.keys(this.state.sessions));
     // return a spinner if component is fetching data
-    if (this.props.loading) {
+    if (this.props.loading || this.state.loading) {
       return <Spinner />;
     }
-
     return (
       <View style={styles.container}>
-        <HeadingText size={1} style={styles._heading}>Posture Report</HeadingText>
-        <View style={styles.dateContainer}>
-          <TouchableOpacity
-            onPress={this.onBackPress}
-          >
-            <View>
-              <Icon
-                name="angle-left"
-                size={40}
-                color="#900"
-                style={styles.icons}
-              />
-            </View>
-          </TouchableOpacity>
-          <HeadingText size={2}>{data.date}</HeadingText>
-          {rightArrow}
-        </View>
-        <View style={styles.chart}>
-          <VictoryPie
-            style={{
-              labels: { fontSize: 14, fontWeight: 'bold' },
-            }}
-            data={data.chartData}
-            innerRadius={80}
-            x="label"
-            y="duration"
-            width={300}
-            padding={{ top: 80, bottom: 0, left: 50, right: 50 }}
-            animate={{
-              duration: 700,
-            }}
-            colorScale={[
-              'white',
-              color.primaryColor,
-            ]}
-          />
-          <View style={styles._total}>
-            <HeadingText size={1}>{data.total ? 'Total' : ''}</HeadingText>
-            <HeadingText size={1}>{data.total}</HeadingText>
-          </View>
-        </View>
-        <View style={styles._statsContainer}>
-          <View style={styles.innerContainer}>
-            <BodyText style={styles._statsGoodHeader}>Good</BodyText>
-            <BodyText style={styles._statsText}>{data.good}</BodyText>
-          </View>
-          <View style={styles.innerContainer}>
-            <BodyText style={styles._statsBadHeader}>Bad</BodyText>
-            <BodyText style={styles._statsText}>{data.bad}</BodyText>
-          </View>
-        </View>
+        <HeadingText size={1} style={styles._heading}>Calendar</HeadingText>
+        <Calendar
+          showEventIndicators
+          showControls
+          onTouchPrev={this.onTouchPrev}
+          onTouchNext={this.onTouchNext}
+          onDateSelect={this.onDateSelect}
+          eventDates={events}
+          customStyle={calendarStyles}
+          startDate={this.state.startDate}
+        />
       </View>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  let seen = {};
-  // Sort the sessions
-  // Merge the all the sessions with the same date
-  const groupByDay = state.user.sessions
-    .sort((a, b) => b.timestamp - a.timestamp) // sort from latest to oldest
-    .filter((val) => {
-      const date = new Date(val.timestamp);
-
-      if (new Date(seen.timestamp).toDateString() === (date.toDateString())) {
-        seen.sessionTime += val.sessionTime;
-        seen.slouchTime += val.slouchTime;
-        seen.totalDuration += val.totalDuration;
-        // Don't keep this value, It's merged
-        return false;
-      }
-
-      // remember this obj
-      seen = val;
-      return true;
-    });
-
-  return {
-    sessions: groupByDay,
-    loading: state.user.isUpdating,
-  };
-};
+const mapStateToProps = (state) => ({
+  sessions: state.user.sessions,
+  loading: state.user.isUpdating,
+});
 
 export default connect(mapStateToProps)(PostureReport);
