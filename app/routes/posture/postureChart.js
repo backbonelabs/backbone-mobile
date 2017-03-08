0a3a8e1605ddca3109b9a8ed79f23882af3ec0be
Miguel Correa <miguelc1221@gmail.com>
2017-03-08 11:55:58 -0500

    change title

--- app/routes/posture/postureChart.js
+++ app/routes/posture/postureChart.js
@@ -3,6 +3,6 @@ import PostureChart from '../../components/posture/postureReport/PostureChart';
 export default {
   name: 'postureChart',
   component: PostureChart,
-  title: 'Posture Chart',
+  title: 'Posture Summary',
   showBackButton: true,
 };
