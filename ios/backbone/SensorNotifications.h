509e4941c7f5f2a186ea94b5b99a2356c3251a3a
Eko Mirhard <eko.mirhard@gmail.com>
2017-03-15 09:10:50 +0700

    Removed unused files physically on iOS

--- ios/backbone/SensorNotifications.h
+++
@@ -1,8 +0,0 @@
-#ifndef SensorNotifications_h
-#define SensorNotifications_h
-
-extern NSString * const AccelerometerNotification;
-extern NSString * const AccelerometerBMI160Notification;
-extern NSString * const GyroscopeNotification;
-
-#endif /* SensorNotifications_h */
