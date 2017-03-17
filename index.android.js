fbd97a03d654f47b036a9168093fc1fdfd2847c4
Kevin Huang <huang.kev@gmail.com>
2017-03-17 13:06:17 -0700

    Add redux-async-action package

--- index.android.js
+++ index.android.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { asyncActionMiddleware } from 'redux-async-action';
 import reducers from './app/reducers';
 import Application from './app/containers/Application';
 import theme from './app/styles/theme';
@@ -12,7 +13,7 @@ EStyleSheet.build(theme);
 
 const store = createStore(
   reducers,
-  applyMiddleware(thunk)
+  applyMiddleware(asyncActionMiddleware, thunk)
 );
 
 const Backbone = () => (
