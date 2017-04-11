c691054765f91a6be7c8d72e61905ce9235ae5af
Miguel Correa <miguelc1221@gmail.com>
2017-04-11 14:17:04 -0400

    add stylesheet

---
+++ app/styles/education/educationContent.js
@@ -0,0 +1,10 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+  titleList: {
+    textAlign: 'center',
+    backgroundColor: '#eee',
+    marginBottom: 2,
+    padding: 10,
+  },
+});
