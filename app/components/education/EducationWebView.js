26a0fff0dacb67521663f2e1bb6cb35afbd1a798
Miguel Correa <miguelc1221@gmail.com>
2017-04-13 12:01:41 -0400

    add spinner and error message

--- app/components/education/EducationWebView.js
+++ app/components/education/EducationWebView.js
@@ -1,7 +1,21 @@
 import React, { PropTypes } from 'react';
 import { WebView } from 'react-native';
+import BodyText from '../BodyText';
+import Spinner from '../Spinner';
 
-const EducationWebView = ({ uri }) => <WebView source={{ uri }} />;
+const showSpinner = () => <Spinner />;
+const showErrorMessage = () => (
+  <BodyText>An error has occur, Please try again later</BodyText>
+);
+
+const EducationWebView = ({ uri }) => (
+  <WebView
+    source={{ uri }}
+    startInLoadingState
+    renderLoading={showSpinner}
+    renderError={showErrorMessage}
+  />
+);
 
 EducationWebView.propTypes = {
   uri: PropTypes.string.isRequired,
