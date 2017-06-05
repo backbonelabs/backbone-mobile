367dd6b01127b451848ba8f5cc2137d2ed8ef329
Miguel Correa <miguelc1221@gmail.com>
2017-06-05 18:32:40 -0400

    refactor input

--- app/components/Input.js
+++ app/components/Input.js
@@ -1,6 +1,8 @@
-import React from 'react';
+
+import React, { Component, PropTypes } from 'react';
 import { TextInput, View } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import styles from '../styles/input';
 
 // This is a map of font names to modules.
@@ -9,63 +11,95 @@ import styles from '../styles/input';
 // the appropriate module.
 const iconMap = {
   FontAwesome: FontAwesomeIcon,
+  MaterialIcon: MaterialIcons,
 };
 
-const Input = (props) => {
-  const {
-    handleRef,
-    style,
-    iconFont,
-    iconRightName,
-    ...remainingProps,
-  } = props;
+class Input extends Component {
+  static propTypes = {
+    editable: PropTypes.bool,
+    handleRef: PropTypes.func,
+    style: PropTypes.object,
+    iconStyle: PropTypes.object,
+    containerStyles: PropTypes.object,
+    iconFont: PropTypes.oneOf(['FontAwesome', 'MaterialIcon']),
+    iconLeftName: PropTypes.string, // maps to a font name in react-native-icons
+  };
 
-  const inputStyles = [styles.inputField];
-  if (props.editable === false) {
-    inputStyles.push(styles.disabled);
+  static defaultProps = {
+    handleRef: () => {},
+    style: {},
+    iconFont: 'FontAwesome',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      borderWidth: 0,
+    };
+
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
   }
-  inputStyles.push(style);
 
-  const Icon = iconMap[iconFont];
+  onFocus() {
+    this.setState({
+      borderWidth: 0.5,
+    });
+  }
 
-  return (
-    <View style={[styles.container, props.containerStyles]}>
-      <TextInput
-        ref={ref => handleRef(ref)}
-        style={inputStyles}
-        placeholderTextColor={styles._$placeholderTextColor}
-        underlineColorAndroid={'transparent'}
-        {...remainingProps}
-      />
-      {Icon && iconRightName ?
-        <Icon
-          name={iconRightName}
-          color={styles.$iconColor}
-          size={styles.$iconSize}
-          style={styles.icon}
+  onBlur() {
+    this.setState({
+      borderWidth: 0,
+    });
+  }
+
+  render() {
+    const {
+      handleRef,
+      style,
+      iconStyle,
+      iconFont,
+      iconLeftName,
+      ...remainingProps,
+    } = this.props;
+
+    const inputStyles = [
+      styles.inputField,
+      { borderWidth: this.state.borderWidth },
+    ];
+    const iconStyles = [styles.icon];
+
+    if (this.props.editable === false) {
+      inputStyles.push(styles.disabled);
+    }
+    inputStyles.push(style);
+    iconStyles.push(iconStyle);
+
+    const Icon = iconMap[iconFont];
+
+    return (
+      <View style={[styles.container, this.props.containerStyles]}>
+        {Icon && iconLeftName
+          ? <Icon
+            name={iconLeftName}
+            color={styles.$iconColor}
+            size={styles.$iconSize}
+            style={iconStyles}
+          />
+          : null}
+        <TextInput
+          ref={ref => handleRef(ref)}
+          style={inputStyles}
+          placeholderTextColor={styles.$placeholderTextColor}
+          underlineColorAndroid={'transparent'}
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+          {...remainingProps}
         />
-        :
-          <View style={{ ...styles._icon, width: styles.$iconSize }} />
-      }
-    </View>
-  );
-};
-
-const { PropTypes } = React;
-
-Input.propTypes = {
-  editable: PropTypes.bool,
-  handleRef: PropTypes.func,
-  style: PropTypes.object,
-  containerStyles: PropTypes.object,
-  iconFont: PropTypes.oneOf(['FontAwesome']),
-  iconRightName: PropTypes.string, // maps to a font name in react-native-icons
-};
-
-Input.defaultProps = {
-  handleRef: () => {},
-  style: {},
-  iconFont: 'FontAwesome',
-};
+      </View>
+    );
+  }
+}
 
 export default Input;
