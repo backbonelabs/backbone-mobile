d8915e53f8869b48a96d38b9770bd0ff2d387e5c
Kevin Huang <huang.kev@gmail.com>
2017-08-05 22:11:33 -0700

    Add Mixpanel event tracking

--- app/actions/training.js
+++ app/actions/training.js
@@ -1,10 +1,23 @@
+import Mixpanel from '../utils/Mixpanel';
 import {
   SELECT_LEVEL,
   SELECT_SESSION,
 } from './types';
+import store from '../store';
+
+const getCurrentTrainingData = () => {
+  const { training } = store.getState();
+  const { selectedPlanIdx, selectedLevelIdx, selectedSessionIdx } = training;
+  return { selectedPlanIdx, selectedLevelIdx, selectedSessionIdx };
+};
 
 export default {
   selectLevel(level) {
+    Mixpanel.trackWithProperties('selectLevel', {
+      ...getCurrentTrainingData(),
+      newLevelIdx: level,
+    });
+
     return {
       type: SELECT_LEVEL,
       payload: level,
@@ -12,6 +25,11 @@ export default {
   },
 
   selectSession(session) {
+    Mixpanel.trackWithProperties('selectSession', {
+      ...getCurrentTrainingData(),
+      newSessionIdx: session,
+    });
+
     return {
       type: SELECT_SESSION,
       payload: session,
