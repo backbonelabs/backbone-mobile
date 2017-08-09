d66da96a9e682ed33e737a85caa560296fcd1106
Eko Mirhard <eko.mirhard@gmail.com>
2017-08-09 20:27:01 +0700

    Separate Toggle component into a separate class for reusability

---
+++ app/styles/toggle.js
@@ -0,0 +1,23 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+import relativeDimensions from '../utils/relativeDimensions';
+
+const { applyWidthDifference } = relativeDimensions;
+
+export default EStyleSheet.create({
+  toggleContainer: {
+    flex: 0.08,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: applyWidthDifference(15),
+    paddingRight: applyWidthDifference(10),
+    borderBottomWidth: 1,
+    borderColor: '#EEEEEE',
+    height: applyWidthDifference(48),
+  },
+  toggleText: {
+    flex: 0.85,
+  },
+  toggleSwitch: {
+    flex: 0.15,
+  },
+});
