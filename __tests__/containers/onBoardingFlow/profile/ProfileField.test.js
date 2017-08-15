023344a051bce33c97d23d5b142102f5bbbdf10c
Eko Mirhard <eko.mirhard@gmail.com>
2017-08-15 18:42:18 +0700

    Simplify ProfileField Test

--- __tests__/containers/onBoardingFlow/profile/ProfileField.test.js
+++ __tests__/containers/onBoardingFlow/profile/ProfileField.test.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import { asyncActionMiddleware } from 'redux-async-action';
 import birthdayIconOff from '../../../../app/images/onboarding/birthday-icon-off.png';
 import birthdayIcon from '../../../../app/images/onboarding/birthday-icon-on.png';
 import ProfileField from '../../../../app/containers/onBoardingFlow/profile/ProfileField';
 
 describe('ProfileField Component', () => {
-  const initialState = {
-    auth: {},
-    user: {},
-  };
-  const store = configureStore([asyncActionMiddleware, thunk])(initialState);
-
   const valEmptyLabel = {
     type: 'birthdate',
     icon: birthdayIcon,
@@ -28,36 +19,34 @@ describe('ProfileField Component', () => {
     <ProfileField
     	value={val}
     	key={val.type}
-    	setPickerType={'birthdate'}
+    	setPickerType={() => {
+        // Empty test function
+      }}
       icon={val.icon}
-    />,
-    { context: { store } },
+    />
   );
   const wrapperEmptyLabel = shallow(
     <ProfileField
     	value={valEmptyLabel}
     	key={valEmptyLabel.type}
-    	setPickerType={'birthdate'}
+    	setPickerType={() => {
+        // Empty test function
+      }}
       icon={valEmptyLabel.icon}
-    />,
-    { context: { store } },
+    />
   );
 
-  let render;
-  beforeEach(() => {
-    store.clearActions();
-    render = wrapper.dive();
-  });
+  let render = wrapper.dive();
 
   test('renders as expected', () => {
     expect(render).toMatchSnapshot();
   });
 
   test('renders a BodyText if label exists', () => {
-  	expect(wrapper.dive().find('BodyText')).toHaveLength(1);
+  	expect(wrapper.find('BodyText')).toHaveLength(1);
   });
 
   test('renders a SecondaryText if no label found', () => {
-    expect(wrapperEmptyLabel.dive().find('SecondaryText')).toHaveLength(1);
+    expect(wrapperEmptyLabel.find('SecondaryText')).toHaveLength(1);
   });
 });
