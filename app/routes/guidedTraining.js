c9b2862aa9ec0d8938f06182ee622a55afeebd51
Tuan Ho <tuan@gobackbone.com>
2017-09-13 00:01:03 -0700

    fix lint

--- app/routes/guidedTraining.js
+++ app/routes/guidedTraining.js
@@ -11,7 +11,8 @@ const GuidedTrainingHelp = props => {
     'Repetitions defines the number of times to perform an exercise. ' +
     'For example, you do 10 squats, then stop. ' +
     'The 10 squats you performed are considered 10 repetitions. \n\n' +
-    'Sets: Sets refers to how many times you will repeat an exercise for the set number of repetitions. ' +
+    'Sets: Sets refers to how many times you will repeat an exercise ' +
+    'for the set number of repetitions. ' +
     'For example, you do 10 squats and rest, and then you do another 10 squats and rest. ' +
     'You have now completed two sets of 10 reps.';
   const showGlossary = () => {
