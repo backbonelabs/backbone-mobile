8cea239a53884f699952e6c3a95fde9118598e94
Tuan Ho <tuan@gobackbone.com>
2017-09-13 20:45:28 -0700

    updated glossary text

--- app/routes/guidedTraining.js
+++ app/routes/guidedTraining.js
@@ -6,20 +6,21 @@ import GuidedTraining from '../containers/GuidedTraining';
 import appActions from '../actions/app';
 import styles from '../styles/guidedTraining';
 
+const glossary = 'Reps: Reps is short for repetitions. ' +
+  'Repetitions refer to the number of times to perform an exercise. ' +
+  'For example, you do 10 squats, then stop. ' +
+  'The 10 squats you performed are considered 10 repetitions. \n\n' +
+  'Sets: Sets refer to how many times you will repeat an exercise ' +
+  'for the recommended number of reps. ' +
+  'For example, you do 10 squats and rest, and then you do another 10 squats and rest. ' +
+  'You have now completed two sets of 10 reps.';
+
 const GuidedTrainingHelp = props => {
-  const glossary = 'Reps: Reps is short for repetitions. ' +
-    'Repetitions defines the number of times to perform an exercise. ' +
-    'For example, you do 10 squats, then stop. ' +
-    'The 10 squats you performed are considered 10 repetitions. \n\n' +
-    'Sets: Sets refers to how many times you will repeat an exercise ' +
-    'for the set number of repetitions. ' +
-    'For example, you do 10 squats and rest, and then you do another 10 squats and rest. ' +
-    'You have now completed two sets of 10 reps.';
   const showGlossary = () => {
     props.showPartialModal({
       title: { caption: 'Glossary' },
       detail: {
-        caption: `${glossary}`,
+        caption: glossary,
       },
       buttons: [{ caption: 'CLOSE' }],
       backButtonHandler: () => {
