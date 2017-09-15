bf4a85de4328db7ffcb50dcf8df6bdb5769d84d4
Tuan Ho <tuan@gobackbone.com>
2017-09-14 17:55:55 -0700

    remove space

--- app/routes/guidedTraining.js
+++ app/routes/guidedTraining.js
@@ -9,7 +9,7 @@ import styles from '../styles/guidedTraining';
 const glossary = 'Reps: Reps is short for repetitions. ' +
   'Repetitions refer to the number of times to perform an exercise. ' +
   'For example, you do 10 squats, then stop. ' +
-  'The 10 squats you performed are considered 10 repetitions. \n\n' +
+  'The 10 squats you performed are considered 10 repetitions.\n\n' +
   'Sets: Sets refer to how many times you will repeat an exercise ' +
   'for the recommended number of reps. ' +
   'For example, you do 10 squats and rest, and then you do another 10 squats and rest. ' +
